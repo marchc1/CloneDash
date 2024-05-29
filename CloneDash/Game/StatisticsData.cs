11a4363e80e6c667e81a34ee712053fca1f910c5
march <106459595+marchc1@users.noreply.github.com>
2024-05-28 19:38:47 -0700

    Create StatisticsData.cs

---
+++ CloneDash/Game/StatisticsData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneDash.Game
+{
+    // WIP
+    public class StatisticsData
+    {
+        public int Misses { get; set; } = 0;
+        public int Hits { get; set; } = 0;
+        public int Avoids { get; set; } = 0;
+
+        public int NeededHits { get; set; } = 0;
+        public int NeededAvoids { get; set; } = 0;
+
+        public bool FullCombo => Misses == 0;
+
+        public int Perfects { get; set; } = 0;
+        public int Greats { get; set; } = 0;
+
+        public List<double> MsAccuracy { get; } = [];
+    }
+}
