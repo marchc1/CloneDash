33adb4aa3c7c3266f258767976e5f0b273b5922f
march <106459595+marchc1@users.noreply.github.com>
2024-05-28 19:40:55 -0700

    Old DashComponents converted to Nucleus LogicalEntities

---
+++ CloneDash/Game/Logic/Conductor.cs
@@ -0,0 +1,137 @@
+﻿using Nucleus;
+using Nucleus.Core;
+using Nucleus.Engine;
+using Nucleus.Types;
+using Nucleus.UI;
+using Raylib_cs;
+
+namespace CloneDash.Game
+{
+    /// <summary>
+    /// Currently unused, and may be a bad idea to try implementing?
+    /// </summary>
+    public struct TempoChange {
+        public double Time;
+        public double BPM;
+
+        public TempoChange(double time, float bpm) {
+            this.Time = time;
+            this.BPM = bpm;
+        }
+    }
+
+    public class Conductor : LogicalEntity {
+        public List<TempoChange> TempoChanges { get; private set; } = [];
+
+        /// <summary>
+        /// The current music playhead, adjusted for inaccuracies.
+        /// </summary>
+        public double Time => currentInaccurateTime;
+
+        /// <summary>
+        /// Offsets the conductor time
+        /// </summary>
+        public double PreStartTime { get; set; } = 5;
+
+        public double BPM => GetTempoAtTime(Time);
+
+        // What was the last music time that Raylib reported?
+        private float lastTimeFromFunctionCall = 0;
+        // What is the current time + the frametime values
+        private float currentInaccurateTime = 0;
+
+        private class CD_Conductor_UIBar : Element
+        {
+            public float Completion { get; set; }
+
+            protected override void Paint(float width, float height) {
+                Graphics2D.SetDrawColor(230, 235, 255);
+                Graphics2D.DrawRectangle(0, 0, width * Completion, height);
+            }
+        }
+
+        private CD_Conductor_UIBar UIBar;
+
+        public override void Initialize() {
+            UIBar = Level.UI.Add<CD_Conductor_UIBar>();
+            UIBar.Dock = Dock.Bottom;
+            UIBar.Size = new(0, 8);
+        }
+        public TempoChange GetTempoChangeAtTime(double time) {
+            if (TempoChanges.Count == 0)
+                throw new Exception("No tempo changes found in DashGame (likely a DashSheet import error)");
+
+            if(TempoChanges.Count == 1)
+                return TempoChanges[0];
+
+            TempoChange last = TempoChanges[0];
+            foreach(var tempoChange in TempoChanges) {
+                if (time >= tempoChange.Time)
+                    last = tempoChange;
+                else
+                    break;
+            }
+
+            return last;
+        }
+        /// <summary>
+        /// Gets the current BPM from the song position
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public double GetTempoAtTime(double time) => GetTempoChangeAtTime(time).BPM;
+
+        public bool firstTick = true;
+        public override void Think(FrameState frameState) {
+            var game = Level.As<CD_GameLevel>();
+            Level.FrameDebuggingStrings.Add($"Conductor Time: {Time}");
+
+            if (firstTick) {
+                currentInaccurateTime = (float)-PreStartTime;
+            }
+            else if (currentInaccurateTime < 0) {
+                var ft = Raylib.GetFrameTime();
+                if (ft > 0.5f)
+                    return;
+
+                currentInaccurateTime += ft;
+            }
+            else {
+                game.Music.Update();
+
+                var now = game.Music.Playhead;
+                var paused = game.Music.Paused;
+
+                if (lastTimeFromFunctionCall != now && !paused) {
+                    lastTimeFromFunctionCall = now;
+                    currentInaccurateTime = now;
+                }
+                else {
+                    if (!paused)
+                        currentInaccurateTime += EngineCore.FrameTime;
+                }
+            }
+
+            UIBar.Completion = game.Music.Playhead / game.Music.Length;
+            firstTick = false;
+        }
+
+        /// <summary>
+        /// Returns how long 1/<paramref name="division"/> of a note takes, in seconds. By default, <paramref name="division"/> is set to 4, which is a quarter note.
+        /// </summary>
+        /// <param name="division"></param>
+        /// <returns></returns>
+        public float NoteDivisorToSeconds(float division = 4) => (60 / (float)BPM) * (4 / division);
+
+        /// <summary>
+        /// Returns a value between zero to one, where zero is the current beat starting, and one is the current beat ending.<br></br>
+        /// <paramref name="division"/> is by default set to 4, which means you'll get a 0-1 value for each quarter note.
+        /// </summary>
+        /// <param name="division"></param>
+        /// <returns></returns>
+        public float NoteDivisorRealtime(float division = 4) {
+            var div2sec = NoteDivisorToSeconds(division);
+            return ((float)Time % div2sec) / div2sec;
+        }
+    }
+}
---
+++ CloneDash/Game/Logic/Pathway.cs
@@ -0,0 +1,90 @@
+﻿using CloneDash.Animation;
+using Nucleus.Core;
+using Nucleus.Engine;
+using Nucleus.Types;
+using Raylib_cs;
+
+namespace CloneDash.Game
+{
+    public class Pathway : LogicalEntity
+    {
+        public bool IsClicked => ValueDependantOnPathway(Side, Level.As<CD_GameLevel>().InputState.TopClicked > 0, Level.As<CD_GameLevel>().InputState.BottomClicked > 0);
+        public bool IsPressed => ValueDependantOnPathway(Side, Level.As<CD_GameLevel>().InputState.TopHeld, Level.As<CD_GameLevel>().InputState.BottomHeld);
+
+        /// <summary>
+        /// The half of the screen the pathway resides on.
+        /// </summary>
+        public PathwaySide Side { get; set; } = PathwaySide.None;
+
+        private bool checkSide(PathwaySide side) {
+            if (Side == PathwaySide.None || Side == PathwaySide.Both)
+                throw new NotImplementedException("A pathway must have a very specific side attached to it.");
+
+            return Side == side;
+        }
+
+        /// <summary>
+        /// Is this the top pathway?
+        /// </summary>
+        public bool IsTopPathway => checkSide(PathwaySide.Top);
+        /// <summary>
+        /// Is this the bottom pathway?
+        /// </summary>
+        public bool IsBottomPathway => checkSide(PathwaySide.Bottom);
+
+        /// <summary>
+        /// The size of the pathway hit marker is changed by both every quarter note and when an input event occurs; this animation smoother is used for input events.
+        /// </summary>
+        public SecondOrderSystem InputAnimator { get; private set; } = new(0.4f, 0.5f, 1f, 1);
+
+        public Pathway(PathwaySide side) : base() {
+            Side = side;
+
+        }
+
+        public static bool ComparePathwayType(PathwaySide a, PathwaySide b) {
+            if (a == b)
+                return true;
+            else if (a == PathwaySide.Both || b == PathwaySide.Both)
+                return true;
+
+            return false;
+        }
+
+        public static T ValueDependantOnPathway<T>(PathwaySide input, T topResult, T bottomResult) {
+            return input == PathwaySide.Top ? topResult : bottomResult;
+        }
+
+        public static Color GetColor(PathwaySide side, int alpha = -1) {
+            var c = ValueDependantOnPathway(side, DashVars.TopPathwayColor, DashVars.BottomPathwayColor);
+
+            return new(c.R, c.G, c.B, alpha == -1 ? c.A : alpha);
+        }
+        public Color Color => GetColor(Side);
+
+        public SecondOrderSystem Animator { get; private set; } = new(8.4f, 0.5f, 1f, 1);
+        public Vector2F Position { get; private set; }
+        public override void Think(FrameState frameState) {
+            Position = new Vector2F(frameState.WindowWidth * DashVars.PATHWAY_XDISTANCE, (frameState.WindowHeight / 2) + (frameState.WindowHeight / 2) * ValueDependantOnPathway(Side, -DashVars.PATHWAY_YDISTANCE, DashVars.PATHWAY_YDISTANCE));
+        }
+        public override void PostRender(FrameState frameState) {
+            var beatInfluence = 1 - Level.As<CD_GameLevel>().Conductor.NoteDivisorRealtime(4);
+            var realInfluence = Animator.Update((IsClicked || IsPressed) ? 2 : beatInfluence);
+            var size = Raymath.Remap(realInfluence, 0, 1, 25, 32);
+            var curtimeOffset = (float)Level.Curtime * 120;
+
+            float divisors = 3;
+            float ring_offset = 60;
+
+            var alpha = (int)Raymath.Remap(realInfluence, 0, 1, 79, 130);
+
+            Graphics2D.SetDrawColor(ValueDependantOnPathway(Side, DashVars.TopPathwayColor, DashVars.BottomPathwayColor), alpha);
+            Graphics2D.DrawRing(Position, (25 / 2) - 3, (25 / 2));
+
+            var ringPartSize = 360f / divisors;
+            for (float i = 0; i < 360f; i += ringPartSize) {
+                Graphics2D.DrawRing(Position, size, size / 1.15f, curtimeOffset + i, curtimeOffset + i + (ringPartSize - ring_offset));
+            }
+        }
+    }
+}
