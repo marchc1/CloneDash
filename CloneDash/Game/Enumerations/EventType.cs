8d319b2b4755b21abdef6668bb0b29fe68aa4d60
march <106459595+marchc1@users.noreply.github.com>
2024-05-28 19:45:09 -0700

    Old enumerations re-added

---
+++ CloneDash/Game/Enumerations/EventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneDash.Game.Enumerations
+{
+    public enum EventType
+    {
+        BossIn,
+        BossOut,
+        BossSingleHit,
+        BossMasher
+    }
+}
