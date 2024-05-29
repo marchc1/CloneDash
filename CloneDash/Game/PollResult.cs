aaec930e8b0257aee84b800d3171f67d8e117917
march <106459595+marchc1@users.noreply.github.com>
2024-05-28 19:39:47 -0700

    Create PollResult.cs

---
+++ CloneDash/Game/PollResult.cs
@@ -0,0 +1,23 @@
+﻿using CloneDash.Game.Entities;
+
+namespace CloneDash.Game
+{
+    public struct PollResult
+    {
+        public bool Hit;
+        public CD_BaseMEntity HitEntity;
+        public double DistanceToHit;
+        public string Greatness;
+
+        public static readonly PollResult Empty = new PollResult() { Hit = false };
+
+        public static PollResult Create(CD_BaseMEntity hitEntity, double distanceToHit, string greatness) {
+            PollResult result = new PollResult();
+            result.Hit = true;
+            result.HitEntity = hitEntity;
+            result.DistanceToHit = distanceToHit;
+            result.Greatness = greatness;
+            return result;
+        }
+    }
+}
