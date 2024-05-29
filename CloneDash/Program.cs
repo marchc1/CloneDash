737859850454f3a293441d7e56339d114a9b6bea
march <106459595+marchc1@users.noreply.github.com>
2024-05-28 19:38:28 -0700

    Update Program.cs

--- CloneDash/Program.cs
+++ CloneDash/Program.cs
@@ -2,76 +2,48 @@
     A *LOT* of this is subject to change. This is a prototype, and just a testbed of basic game functionality.
 */
 
+using CloneDash.Game;
+using CloneDash.Game.Sheets;
 using CloneDash.Systems;
+using Nucleus;
+using Nucleus.Core;
 using Raylib_cs;
 
 namespace CloneDash
 {
+
+    // I've been testing with these levels:
+    /*
+        8bit_adventurer_map3
+        bass_telekinesis_map3
+        can_i_friend_you_on_bassbook_lol_map3
+        night_of_knights_map3
+        hg_makaizou_polyvinyl_shounen_map3
+
+        kyouki_ranbu_map3
+        ourovoros_map3
+        mujinku_vacuum_track_add8e6_map3
+        the_89s_momentum_map3
+    */
+
     internal class Program
     {
-        static string TurnMDAssetBundlePathToMapName(string filepath) {
-            string ret = Path.GetFileNameWithoutExtension(filepath).Replace("noteasset_assets_", "");
-            return ret.Substring(0, ret.LastIndexOf("_"));
-        }
-
         static void Main(string[] args) {
             MuseDashCompatibility.InitializeCompatibilityLayer();
-            
-            string requestedMap = "";
-            string realMapName = "";
 
-            while (true) {
-                Console.Write("Type a Muse Dash map name: ");
-                requestedMap = Console.ReadLine();
+            EngineCore.Initialize(1600, 900, "Clone Dash");
+            EngineCore.GameInfo = new() {
+                GameName = "Clone Dash"
+            };
 
-                string[] mapMatches = Array.FindAll(MuseDashCompatibility.StreamingFiles.ToArray(), x => x.Contains("noteasset") && x.ToLower().Contains(requestedMap.Replace(" ", "_")));
-                
-                if (mapMatches.Length > 0) {
-                    Console.WriteLine("The following maps were found:");
-                    for (int i = 0; i < mapMatches.Length; i++) {
-                        Console.WriteLine($"    [{i + 1}]: {TurnMDAssetBundlePathToMapName(mapMatches[i])}");
-                    }
+            EngineCore.LoadLevel(new CD_MainMenu());
+            // need a better way to implement custom scenes
+            Filesystem.AddPath("audio", Filesystem.Resolve("game") + "assets\\scenes\\default\\audio\\");
+            Filesystem.AddPath("models", Filesystem.Resolve("game") + "assets\\scenes\\default\\models\\");
+            Filesystem.AddPath("scripts", Filesystem.Resolve("game") + "assets\\scenes\\default\\scripts\\");
 
-                    while (true) {
-                        int index = 0;
-                        Console.WriteLine($"\r\nType the number of the map you wish to load");
-                        if (int.TryParse(Console.ReadLine(), out index)) {
-                            if (!DashMath.InRange(index, 1, mapMatches.Length))
-                                Console.WriteLine($"Index out of range (needs a value between 1 -> {mapMatches.Length}");
-                            else {
-                                realMapName = mapMatches[index - 1];
-                                break;
-                            }
-                        }
-                        else
-                            Console.WriteLine("Input is not a number, try again");
-                    }
-
-                    break;
-                }
-                else {
-                    Console.WriteLine("No maps found, try again");
-                }
-            }
-
-            Raylib.InitAudioDevice();
-            Raylib.SetTraceLogLevel(TraceLogLevel.LOG_WARNING);
-            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
-            Raylib.InitWindow((int)1600, (int)900, "Clone Dash");
-            Raylib.SetExitKey(KeyboardKey.KEY_NULL);
-            TextureSystem.LoadTextures();
-            GameLogic.Startup(realMapName);
-
-
-            while (!Raylib.WindowShouldClose()) {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.BLACK);
-                //game.Draw();
-                Graphics.SetDrawColor(255, 255, 255);
-                GameLogic.Tick();
-                ConsoleSystem.Draw();
-                Raylib.EndDrawing();
-            }
+            while (!EngineCore.Closing) 
+                EngineCore.Frame();
         }
     }
 }
