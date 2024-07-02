0e4fdfafd1d1a7ee399548e0d99ad15f3b1d1d44
march <106459595+marchc1@users.noreply.github.com>
2024-07-02 15:05:25 -0700

    UI improvements from other branch

--- Nucleus/UI/UserInterface.cs
+++ Nucleus/UI/UserInterface.cs
@@ -1,4 +1,5 @@
-﻿using Nucleus.Types;
+﻿using Nucleus.Core;
+using Nucleus.Types;
 using Raylib_cs;
 
 namespace Nucleus.UI
@@ -9,14 +10,26 @@ namespace Nucleus.UI
         public Element? Hovered;
         public Element? Depressed;
 
-        public List<Element> Popups = [];
+        //public List<Element> Popups = [];
+        public List<Element> Popups { get; private set; } = [];
         public List<Element> Elements { get; private set; } = [];
         public bool PopupActive => Popups.Count > 0;
+
+        public void RemovePopup(Element e) {
+            Popups.Remove(e);
+        }
+
+        public UserInterface() {
+            Preprocess(new FrameState() {
+                WindowWidth = Raylib.GetScreenWidth(),
+                WindowHeight = Raylib.GetScreenHeight()
+            });
+        }
 
         protected override void Initialize() {
             UI = this;
 
-            OnThink(new FrameState() {
+            Preprocess(new FrameState() {
                 WindowWidth = Raylib.GetScreenWidth(),
                 WindowHeight = Raylib.GetScreenHeight()
             });
@@ -31,18 +44,61 @@ namespace Nucleus.UI
             }
         }
 
+        public override void PostRenderChildren() {
+            var text = TooltipText;
+            if (text != "" && text != null) {
+                var font = "Segoe UI";
+                var fontsize = 20;
+                var size = Graphics2D.GetTextSize(text, font, fontsize) + new Vector2F(8, 4);
+                var mousepos = EngineCore.CurrentFrameState.MouseState.MousePos + new Vector2F(8);
+                Graphics2D.SetDrawColor(50, 57, 65, 120);
+                Graphics2D.DrawRectangle(mousepos, size);
+                Graphics2D.SetDrawColor(10, 15, 25, 225);
+                Graphics2D.SetDrawColor(235, 235, 235, 255);
+                Graphics2D.DrawRectangleOutline(mousepos, size + new Vector2F(4, 4), 1);
+                Graphics2D.DrawText(mousepos + new Vector2F(6, 4), text, font, fontsize);
+            }
+        }
+
         protected override void OnThink(FrameState frameState) {
             Clipping = false;
             //this.Position = new(0, 0);
             //this.Size = new(frameState.WindowWidth, frameState.WindowHeight);
             //RenderBounds = RectangleF.FromPosAndSize(this.Position, this.Size);
         }
+
         internal override void SetupLayout() {
             LayoutInvalidated = false;
         }
 
         public override void MouseClick(FrameState state, Types.MouseButton button) {
             EngineCore.KeyboardUnfocus(this, true);
+        }
+
+        private string _tooltipText = "";
+        public override string TooltipText {
+            get {
+                if (Hovered != null && Hovered != this) {
+                    return Hovered.TooltipText;
+                }
+                return _tooltipText;
+            }
+            set {
+                _tooltipText = value;
+            }
+        }
+
+        public override void Center() {
+            var screen = Raylib.GetCurrentMonitor();
+            var mpos = Raylib.GetMonitorPosition(screen).ToNucleus();
+            var msize = new Vector2F(Raylib.GetMonitorWidth(screen), Raylib.GetMonitorHeight(screen));
+
+            var mposCenter = mpos + (msize / 2);
+            var mposFinal = mposCenter - new Vector2F(Raylib.GetScreenWidth() / 2f, Raylib.GetScreenHeight() / 2f);
+            EngineCore.SetWindowPosition(mposFinal);
+        }
+        ~UserInterface() {
+            MainThread.RunASAP(Remove);
         }
     }
 }
