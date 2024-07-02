1fe350fd3452b7246fa0383ae94a4fe995ecc0a8
march <106459595+marchc1@users.noreply.github.com>
2024-07-02 16:49:37 -0700

    Separate MouseButton to its own file

---
+++ Nucleus/Types/MouseButton.cs
@@ -0,0 +1,20 @@
+﻿namespace Nucleus.Types
+{
+    public record MouseButton(int Button)
+    {
+        public static MouseButton Mouse1 { get; } = new(1);
+        public static MouseButton MouseLeft { get; } = new(1);
+
+        public static MouseButton Mouse2 { get; } = new(2);
+        public static MouseButton MouseRight { get; } = new(2);
+
+        public static MouseButton Mouse3 { get; } = new(3);
+        public static MouseButton MouseMiddle { get; } = new(3);
+
+        public static MouseButton Mouse4 { get; } = new(4);
+        public static MouseButton MouseBack { get; } = new(4);
+
+        public static MouseButton Mouse5 { get; } = new(5);
+        public static MouseButton MouseForward { get; } = new(5);
+    }
+}
