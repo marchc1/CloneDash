3fe6b57a39c6a503c90758b674e2e881d7db97c2
march <106459595+marchc1@users.noreply.github.com>
2024-07-02 15:09:03 -0700

    Update Engine

--- Nucleus/Engine/EngineCore.cs
+++ Nucleus/Engine/EngineCore.cs
@@ -1,16 +1,62 @@
 ﻿using CloneDash.Systems;
 using Nucleus.Core;
+using Nucleus.CrossPlatform;
 using Nucleus.Engine;
 using Nucleus.Types;
 using Nucleus.UI;
 using Nucleus.UI.Elements;
 using Raylib_cs;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Nucleus
 {
+    public enum ThreadExecutionTime
+    {
+        BeforeFrame,
+        AfterFrameStateConstructed,
+        AfterThink,
+        AfterFrame
+    }
+    public record MainThreadExecutionTask(Action Action, ThreadExecutionTime When);
+
+    /// <summary>
+    /// A way to run parameterless and returnless methods/anonymous delegates on the main thread.
+    /// </summary>
+    /// 
+    public static class MainThread
+    {
+        private static Thread? _thread = null;
+
+        public static Thread Thread {
+            get => _thread ?? throw new Exception("For some reason, the MainThread.Thread property was accessed before it was set. EngineCore initialization sets this variable.");
+            set {
+                if (_thread != null)
+                    throw new Exception("The MainThread.Thread property can not be set again.");
+                _thread = value;
+            }
+        }
+        private static ConcurrentQueue<MainThreadExecutionTask> Actions { get; } = [];
+
+        public static void RunASAP(Action a, ThreadExecutionTime when = ThreadExecutionTime.BeforeFrame) => Actions.Enqueue(new(a, when));
+
+        public static void Run(ThreadExecutionTime when) {
+            lock (Actions) {
+                List<MainThreadExecutionTask> putBack = [];
+                while (Actions.TryDequeue(out var task)) {
+                    if (task.When == when)
+                        task.Action();
+                    else
+                        putBack.Add(task);
+                }
+
+                foreach (var task in putBack)
+                    Actions.Enqueue(task);
+            }
+        }
+    }
     public static class EngineCore
     {
         // ------------------------------------------------------------------------------------------ //
@@ -20,7 +66,7 @@ namespace Nucleus
         /// <summary>
         /// A special loading screen level. Always kept "loaded" in some fashion, although only runs its frame function when LoadingLevel == true.
         /// </summary>
-        public static Level LoadingScreen { get; private set; }
+        public static Level LoadingScreen { get; set; }
         /// <summary>
         /// The current level; if null, you'll get a big red complaint
         /// </summary>
@@ -99,23 +145,63 @@ namespace Nucleus
             Logs.Source = "nucleus";
         }
 
-        public static void Initialize(int windowWidth, int windowHeight, string windowName = "Nucleus Engine") {
-            ConsoleSystem.Initialize();
+        public static MouseCursor MouseCursor_Frame { get; private set; }
+        public static MouseCursor? MouseCursor_Persist { get; private set; }
 
+        public static void SetMouseCursor(MouseCursor? mouseCursor, bool persist = false) {
+            if (persist) {
+                MouseCursor_Persist = mouseCursor;
+            }
+            else {
+                if (mouseCursor.HasValue)
+                    MouseCursor_Frame = mouseCursor.Value;
+            }
+        }
+
+        public static void ResetMouseCursor() {
+            MouseCursor_Frame = MouseCursor.MOUSE_CURSOR_DEFAULT;
+            MouseCursor_Persist = null;
+        }
+
+        public static void Initialize(int windowWidth, int windowHeight, string windowName = "Nucleus Engine", string? icon = null, ConfigFlags[]? flags = null) {
+            MainThread.Thread = Thread.CurrentThread;
+            // check build number, 3rd part is days since jan 1st, 2000
+            ConsoleSystem.Initialize();
+            Console.Title = windowName;
+
+            var version = new DateTime(2000, 1, 1) + TimeSpan.FromDays(typeof(EngineCore).Assembly.GetName().Version.Build);
             var isDebug = IsAssemblyDebugBuild(typeof(EngineCore).Assembly);
-            Logs.Info($"Nucleus Engine, Build {(isDebug ? "[DEBUG]" : "[RELEASE]")}.");
+            Logs.Info($"Nucleus Engine, Build {(isDebug ? "[DEBUG]" : "[RELEASE]")} {version.Month}-{version.Day}-{version.Year}.");
             Logs.Info("Initializing...");
             unsafe {
                 Raylib.SetTraceLogCallback(&LogCustom);
             }
+
+            ConfigFlags add = (ConfigFlags)0;
+            bool undecorated = false;
+
+            if (flags != null) {
+                foreach (var flag in flags) {
+                    add |= flag;
+                    if (flag == ConfigFlags.FLAG_WINDOW_UNDECORATED) {
+                        undecorated = true;
+                        //add |= ConfigFlags.FLAG_WINDOW_TRANSPARENT;
+                    }
+                }
+            }
+
             Raylib.InitAudioDevice();
             Raylib.SetTraceLogLevel(TraceLogLevel.LOG_WARNING);
-            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
+            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE | add);
             Raylib.InitWindow(windowWidth, windowHeight, windowName);
+            if (icon != null) Raylib.SetWindowIcon(Raylib.LoadImage(Filesystem.Resolve(icon, "images")));
+            OpenGL.Import(OpenGLAddressRetriever.GetProc);
             Raylib.SetExitKey(Raylib_cs.KeyboardKey.KEY_NULL);
         }
+
         private static Window console;
         private static Textbox lines;
+
         private static void OpenConsole() {
             if (IValidatable.IsValid(console)) {
                 console.Remove();
@@ -146,7 +232,7 @@ namespace Nucleus
             ConsoleSystem.ConsoleMessageWrittenEvent += ConsoleSystem_ConsoleMessageWrittenEvent;
 
             lines.Text = "";
-            foreach(ConsoleMessage message in ConsoleSystem.GetMessages()) {
+            foreach (ConsoleMessage message in ConsoleSystem.GetMessages()) {
                 lines.Text += message.Message + "\n";
             }
 
@@ -170,7 +256,8 @@ namespace Nucleus
             }
 
             Model3AnimationChannel.GlobalPause = false;
-            AudioSystem.Reset();
+            AudioSystem.Unload();
+            TextureSystem.Unload();
 
             Level = level;
             LoadingLevel = true;
@@ -180,18 +267,22 @@ namespace Nucleus
             }));
             LoadingLevel = false;
 
+            LoadingScreen?.Unload();
             __nextFrameLevel = null;
             __nextFrameArgs = null;
-            GC.Collect();
+            //GC.Collect();
+            //GC.WaitForPendingFinalizers();
         }
 
         private static Level __nextFrameLevel;
         private static object[] __nextFrameArgs;
+        public static bool Started { get; private set; } = false;
         public static bool InLevelFrame { get; private set; } = false;
         public static void LoadLevel(Level level, params object[] args) {
-            if (InLevelFrame) {
+            if (InLevelFrame || !Started) {
                 __nextFrameLevel = level;
                 __nextFrameArgs = args;
+                LoadingScreen?.Initialize([]);
             }
             else
                 __loadLevel(level, args);
@@ -203,6 +294,7 @@ namespace Nucleus
             if (Level != null) {
                 Level.Unload();
                 Level.UI.Remove();
+                LoadingScreen.Unload();
             }
 
             TextureSystem.Unload();
@@ -210,23 +302,78 @@ namespace Nucleus
             LoadingLevel = false;
         }
 
-        public static bool Closing => Raylib.WindowShouldClose();
+        private static bool _running = true;
+        private static bool _blockClosure = false;
+
+        public static bool Running {
+            get => _running;
+        }
+
+        public delegate void ShouldEngineCloseD();
+        /// <summary>
+        /// Returning true means that the engine close is blocked. False will not block the engine closure.
+        /// </summary>
+        public static event ShouldEngineCloseD? ShouldEngineClose;
+
+        public static void BlockClosure() {
+            _blockClosure = true;
+        }
+
+        public static void Close(bool forced = false) {
+            if (!_running) return;
+
+            if (forced) {
+                _running = false;
+                return;
+            }
+
+            _blockClosure = false;
+            ShouldEngineClose?.Invoke();
+            Level?.ShouldEngineClose();
+            if (_blockClosure == false) {
+                _running = false;
+            }
+        }
+
+        public static Vector2F GetScreenBounds() {
+            Vector2F ret = new Vector2F(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+            if (IsUndecorated && !Maximized)
+                ret -= new Vector2F(8);
+            return ret;
+        }
+        public static Vector2F GetGlobalScreenOffset() {
+            if (!IsUndecorated || Maximized)
+                return Vector2F.Zero;
+
+            return new(4);
+        }
 
         public static FrameState CurrentFrameState { get; set; }
         public static GameInfo GameInfo { get; set; }
 
         public static float FrameTime => Raylib.GetFrameTime();
 
+        public static bool ShowConsoleLogsInCorner { get; set; } = true;
+        public static bool ShowDebuggingInfo { get; set; } = true;
+        public static string FrameCost { get; set; }
+        public static float FrameCostMS { get; set; }
+        public static float FPS => Raylib.GetFPS();
+
         public static void Frame() {
+            MouseCursor_Frame = MouseCursor.MOUSE_CURSOR_DEFAULT;
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(new Color(0, 0, 0, 0));
+            Raylib.ClearBackground(new Color(0, 0, 0, 255));
+            Graphics2D.SetOffset(GetGlobalScreenOffset());
+
+            var screenBounds = GetScreenBounds();
+
             InLevelFrame = true;
             if (LoadingLevel) {
                 if (LoadingScreen == null) {
                     Graphics2D.SetDrawColor(10, 15, 20);
-                    Graphics2D.DrawRectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+                    Graphics2D.DrawRectangle(0, 0, screenBounds.W, screenBounds.H);
                     Graphics2D.SetDrawColor(240, 245, 255);
-                    Graphics2D.DrawText(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2, "LOADING", "Arial", 24, TextAlignment.Center, TextAlignment.Top);
+                    Graphics2D.DrawText(screenBounds.W / 2, screenBounds.H / 2, "LOADING", "Arial", 24, TextAlignment.Center, TextAlignment.Top);
                 }
                 else {
                     LoadingScreen.Frame();
@@ -239,15 +386,81 @@ namespace Nucleus
                 Graphics2D.SetDrawColor(30, 5, 0);
                 Graphics2D.DrawRectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
                 Graphics2D.SetDrawColor(240, 70, 60);
-                Graphics2D.DrawText(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2, "No level loaded or in the process of loading!", "Arial", 24, TextAlignment.Center, TextAlignment.Bottom);
-                Graphics2D.DrawText(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2, "Make sure you're changing EngineCore.Level.", "Arial", 18, TextAlignment.Center, TextAlignment.Top);
+                Graphics2D.DrawText(screenBounds.X / 2, screenBounds.Y / 2, "No level loaded or in the process of loading!", "Arial", 24, TextAlignment.Center, TextAlignment.Bottom);
+                Graphics2D.DrawText(screenBounds.X / 2, screenBounds.Y / 2, "Make sure you're changing EngineCore.Level.", "Arial", 18, TextAlignment.Center, TextAlignment.Top);
             }
+            if (!EngineCore.Running) {
+                Raylib.CloseWindow();
+                return;
+            }
+
             InLevelFrame = false;
             if (__nextFrameLevel != null) {
                 __loadLevel(__nextFrameLevel, __nextFrameArgs);
                 __nextFrameLevel = null;
             }
             Raylib.EndDrawing();
+            Raylib.SetMouseCursor(MouseCursor_Persist ?? MouseCursor_Frame);
+
+            if (Raylib.WindowShouldClose()) {
+                Close();
+            }
+        }
+
+        public static bool Maximized => Raylib.IsWindowMaximized();
+        public static bool Minimized => Raylib.IsWindowMinimized();
+        public static bool Focused => Raylib.IsWindowFocused();
+        public static bool InFullscreen => Raylib.IsWindowFullscreen();
+        public static bool IsUndecorated => Raylib.IsWindowState(ConfigFlags.FLAG_WINDOW_UNDECORATED);
+
+        public static void Maximize() {
+            if (Maximized) {
+                Unmaximize();
+                return;
+            }
+            Raylib.MaximizeWindow();
+        }
+        public static void Unmaximize() {
+            if (!Maximized)
+                return;
+            Raylib.RestoreWindow();
+        }
+        public static void Minimize() {
+            if (Minimized) {
+                Unminimize();
+                return;
+            }
+            Raylib.MinimizeWindow();
+        }
+        public static void Unminimize() {
+            if (!Minimized)
+                return;
+            Raylib.RestoreWindow();
+        }
+
+        public static unsafe Vector2F MousePos {
+            get => GlobalMousePointerRetriever.GetMousePos();
+        }
+
+        public static void LimitFramerate(int v) {
+            Raylib.SetTargetFPS(v);
+        }
+
+        public static void Start() {
+            Started = true;
+            LoadingScreen?.Initialize([]);
+            while (Running) {
+                Frame();
+            }
+            Logs.Info("Nucleus Engine has halted peacefully.");
+        }
+
+        public static Vector2F GetWindowSize() {
+            return new(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+        }
+
+        public static void SetWindowPosition(Vector2F mposFinal) {
+            Raylib.SetWindowPosition((int)mposFinal.X, (int)mposFinal.Y);
         }
     }
 }
