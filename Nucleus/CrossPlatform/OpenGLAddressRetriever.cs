232268a7343df30e4b4413d211c5a4d319e96820
march <106459595+marchc1@users.noreply.github.com>
2024-07-02 14:50:11 -0700

    Separate Windows' methods for OpenGL addresses
    Linux support needed here...

---
+++ Nucleus/CrossPlatform/OpenGLAddressRetriever.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.CrossPlatform
+{
+    public static class OpenGLAddressRetriever
+    {
+#if COMPILED_WINDOWS
+
+        [DllImport("kernel32", CallingConvention = CallingConvention.Winapi, SetLastError = true, CharSet = CharSet.Ansi)]
+        private static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32", CallingConvention = CallingConvention.Winapi, SetLastError = true, CharSet = CharSet.Ansi)]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        [DllImport("opengl32.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern IntPtr wglGetProcAddress(string procedureName);
+
+        public static IntPtr GetProc(string funcName) {
+            //Console.WriteLine($"funcName: {funcName}");
+            var ret = wglGetProcAddress(funcName);
+            if (ret == IntPtr.Zero)
+                ret = GetProcAddress(LoadLibrary("opengl32.dll"), funcName);
+
+            if (ret == IntPtr.Zero)
+                throw new Exception($"no {funcName}");
+
+            return ret;
+        }
+#endif
+    }
+}
