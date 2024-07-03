b8ba9ad2e2555c71e37f403977a11860da496498
march <106459595+marchc1@users.noreply.github.com>
2024-07-02 17:32:38 -0700

    Make button more interactive

--- Nucleus/UI/Elements/Button.cs
+++ Nucleus/UI/Elements/Button.cs
@@ -1,4 +1,7 @@
 ﻿using Nucleus.Core;
+using Nucleus.Types;
+using Raylib_cs;
+
 
 namespace Nucleus.UI
 {
@@ -7,16 +10,31 @@ namespace Nucleus.UI
         protected override void Initialize() {
             base.Initialize();
         }
+        protected override void OnThink(FrameState frameState) {
+            if (Hovered)
+                EngineCore.SetMouseCursor(MouseCursor.MOUSE_CURSOR_POINTING_HAND);
+            if (TriggeredWhenEnterPressed && frameState.KeyboardState.KeyPressed(KeyboardLayout.USA.Enter)) {
+                MouseReleaseOccur(frameState, Types.MouseButton.MouseLeft, true);
+            }
+        }
+
+        public bool TriggeredWhenEnterPressed { get; set; } = false;
+
         public override void Paint(float width, float height) {
-            var back = MixColorBasedOnMouseState(this, BackgroundColor, new(0, 0.8f, 1.8f, 1f), new(0, 1.2f, 0.6f, 1f));
+            var backpre = BackgroundColor;
+            if (TriggeredWhenEnterPressed) {
+                backpre = backpre.Adjust(0, 0, 1 + (Math.Sin(Lifetime * 6) * 1.3));
+            }
+            var back = MixColorBasedOnMouseState(this, backpre, new(0, 0.8f, 1.8f, 1f), new(0, 1.2f, 0.6f, 1f));
             var fore = MixColorBasedOnMouseState(this, ForegroundColor, new(0, 0.8f, 1.8f, 1f), new(0, 1.2f, 0.6f, 1f));
 
             Graphics2D.SetDrawColor(back);
             Graphics2D.DrawRectangle(0, 0, width, height);
             ImageDrawing();
-            Graphics2D.SetDrawColor(fore);
-            Graphics2D.DrawRectangleOutline(0, 0, width, height, 2);
-
+            if (BorderSize > 0) {
+                Graphics2D.SetDrawColor(fore);
+                Graphics2D.DrawRectangleOutline(0, 0, width, height, BorderSize);
+            }
             base.Paint(width, height);
         }
     }
