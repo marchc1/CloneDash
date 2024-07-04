de81a79ab81097f0d41ae5923909f7d97f0c38cd
march <106459595+marchc1@users.noreply.github.com>
2024-07-03 17:00:27 -0700

    Add support for Unicode

--- Nucleus/Core/FontManager.cs
+++ Nucleus/Core/FontManager.cs
@@ -1,14 +1,33 @@
 ﻿using Raylib_cs;
+using System.Runtime.InteropServices;
 
 namespace Nucleus.Core
 {
     public class FontManager
     {
+        private int[] RegisteredCodepoints = [];
+        private HashSet<int> RegisteredCodepointsHash = new HashSet<int>();
+
         public Dictionary<string, string> FontNameToFilepath = new();
         private Dictionary<string, Dictionary<int, Font>> fonttable = new();
 
-        public FontManager(Dictionary<string, string> fonttable) {
+        public void RegisterCodepoint(int c) {
+            if (RegisteredCodepointsHash.Add(c)) {
+                RegisteredCodepoints = RegisteredCodepointsHash.ToArray();
+            }
+        }
+
+        public void RegisterCodepoints(string charsIn) {
+            for (int i = 0; i < charsIn.Length; i += char.IsSurrogatePair(charsIn, i) ? 2 : 1) {
+                RegisterCodepoint(char.ConvertToUtf32(charsIn, i));
+            }
+        }
+
+        public FontManager(Dictionary<string, string> fonttable, string[]? codepoints = null) {
+            codepoints = codepoints ?? [];
             FontNameToFilepath = fonttable;
+            foreach (var codepointStr in codepoints)
+                RegisterCodepoints(codepointStr);
         }
 
         public Font this[string name, int size] {
@@ -21,7 +40,8 @@ namespace Nucleus.Core
                 }
 
                 if (!f1.TryGetValue(size, out font)) {
-                    f1[size] = Raylib.LoadFontEx(FontNameToFilepath[name], size, null, 0);
+
+                    f1[size] = Raylib.LoadFontEx(FontNameToFilepath[name], size, RegisteredCodepoints, RegisteredCodepoints.Length);
                     font = f1[size]; // how did I miss this
                 }
 
