7c8e52c403aad3247b5a0b8f21fd31731a91b6bb
marchc1 <106459595+marchc1@users.noreply.github.com>
2024-07-08 18:16:14 -0700

    Make global mouse position retriever cross-platform for Linux and Windows

--- Nucleus/CrossPlatform/GlobalMousePointerRetriever.cs
+++ Nucleus/CrossPlatform/GlobalMousePointerRetriever.cs
@@ -11,26 +11,73 @@ namespace Nucleus.CrossPlatform
 {
     public static class GlobalMousePointerRetriever
     {
-#if COMPILED_WINDOWS
         [StructLayout(LayoutKind.Sequential)]
         private struct POINT
         {
             public int X;
             public int Y;
+
+            public POINT(int x, int y) {
+                X = x;
+                Y = y;
+            }
         }
-        [DllImport("user32.dll")]
-        private static extern bool ScreenToClient(IntPtr hWnd, ref POINT lpPoint);
-        [DllImport("user32.dll")]
-        private static extern bool GetCursorPos(out POINT lpPoint);
+#if COMPILED_WINDOWS
+        [DllImport("user32.dll")] private static extern bool ScreenToClient(IntPtr hWnd, ref POINT lpPoint);
+        [DllImport("user32.dll")] private static extern bool GetCursorPos(out POINT lpPoint);
+#endif
+#if COMPILED_LINUX
+        [DllImport("libX11.so")] private static extern IntPtr XOpenDisplay(IntPtr display);
+        [DllImport("libX11.so")] private static extern int XQueryPointer(
+            IntPtr display, IntPtr w, out IntPtr root_return, out IntPtr child_return, out int root_x_return, out int root_y_return,
+            out int win_x_return, out int win_y_return, out uint mask_return);
+        [DllImport("libX11.so")] private static extern IntPtr XRootWindow(IntPtr display, int screen_number);
+
+        private static IntPtr display;
+        private static bool __initialized = false;
+        private static void init(){
+            if(__initialized) return;
+
+            display = XOpenDisplay(IntPtr.Zero);
+            if(display == IntPtr.Zero){
+                throw new Exception("Unable to open X11 display.");
+            }
+
+            __initialized = true;
+        }
+
+        private static IntPtr Display {
+            get {
+                init();
+                return display;
+            }
+        }
+
+        private static POINT GetMousePosX11(){
+            IntPtr root = XRootWindow(Display, 0);
+            int rootX, rootY, winX, winY;
+            IntPtr rootReturn, childReturn;
+            uint maskReturn;
+
+            if(XQueryPointer(Display, root, out rootReturn, out childReturn, out rootX, out rootY, out winX, out winY, out maskReturn) == 0){
+                Logs.Warn("Unable to query the pointer position from X11!");
+                return new(0, 0);
+            }
+
+            return new(rootX, rootY);
+        }
 #endif
 
+
         public static unsafe Vector2F GetMousePos() {
-#if COMPILED_WINDOWS 
+#if COMPILED_WINDOWS
             GetCursorPos(out var pt);
             ScreenToClient((nint)Raylib.GetWindowHandle(), ref pt);
             return new Vector2F(pt.X, pt.Y);
-#else
-            throw new NotImplementedException("No OS platform support for MousePos!");
+#endif
+#if COMPILED_LINUX
+            var ret = GetMousePosX11();
+            return new(ret.X, ret.Y);
 #endif
         }
     }
