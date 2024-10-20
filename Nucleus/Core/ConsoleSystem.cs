14b9e9ba61cd1baccde36d141fa6d438aa0aafa2
march <106459595+marchc1@users.noreply.github.com>
2024-10-19 20:24:30 -0700

    Better command line stuff (WIP)

--- Nucleus/Core/ConsoleSystem.cs
+++ Nucleus/Core/ConsoleSystem.cs
@@ -1,7 +1,196 @@
 ﻿using Nucleus.Core;
+using System.Globalization;
 
 namespace Nucleus
 {
+    public enum ConsoleFlags : ulong
+    {
+        None = 0,
+        Saved = 1
+    }
+    public struct CVValue
+    {
+        public string String;
+        public int Length => String.Length;
+        public double? AsDouble;
+        public int? AsInt;
+        public static bool Update(ref CVValue cv, string input) {
+            var different = cv.String != input;
+            cv.String = input;
+            if (double.TryParse(input, out double d)) {
+                cv.AsDouble = d;
+                if (int.TryParse(input, out int i))
+                    cv.AsInt = i;
+                else
+                    cv.AsInt = Convert.ToInt32(Math.Round(d));
+            }
+            else {
+                cv.AsDouble = null;
+                cv.AsInt = null;
+            }
+
+            return different;
+        }
+        public static bool Update(ref CVValue cv, string input, double? min, double? max) {
+            var updated = Update(ref cv, input);
+            var clampChanged = Clamp(ref cv, min, max);
+            return updated || clampChanged;
+        }
+        public static bool Clamp(ref CVValue cv, double? min, double? max) {
+            if (cv.AsDouble == null) return false;
+
+            var oldD = cv.AsDouble;
+            var oldI = cv.AsInt;
+
+            if (min.HasValue) cv.AsDouble = Math.Max(cv.AsDouble.Value, min.Value);
+            if (max.HasValue) cv.AsDouble = Math.Min(cv.AsDouble.Value, max.Value);
+
+            cv.AsInt = Convert.ToInt32(Math.Round(cv.AsDouble.Value));
+
+            return cv.AsDouble != oldD || cv.AsInt != oldI;
+        }
+    }
+
+    public abstract class ConCommandBase
+    {
+        protected static Dictionary<string, ConCommandBase> lookup { get; } = [];
+
+        public delegate void ChangeCallback(ConVar self, CVValue old, CVValue now);
+
+        public string Name { get; private set; } = "";
+        public string HelpString { get; private set; } = "";
+        public ConsoleFlags Flags { get; private set; }
+
+        protected ConCommandBase() {
+
+        }
+        public ConCommandBase(string name, string helpString = "", ConsoleFlags flags = ConsoleFlags.None) {
+            if (IsRegistered(name))
+                throw new Exception($"ConCommandBase: {name} already exists");
+
+            Name = name;
+            HelpString = helpString;
+            Flags = flags;
+
+            Register(name, this);
+        }
+
+        public static bool IsRegistered(string name) => lookup.ContainsKey(name);
+        public static ConCommandBase? Get(string name) {
+            if (IsRegistered(name))
+                return lookup[name];
+
+            return null;
+        }
+
+        public static void Register(string name, ConCommandBase cmd) {
+            if (IsRegistered(name))
+                return;
+            lookup.Add(name, cmd);
+        }
+
+        public abstract bool IsCommand { get; }
+        public virtual bool IsFlagSet(ConsoleFlags flag) => (flag & Flags) == flag;
+        public virtual void AddFlags(ConsoleFlags flags) => Flags = Flags | flags;
+        public virtual void RemoveFlags(ConsoleFlags flags) => Flags = Flags & ~flags;
+    }
+    public class ConCommand : ConCommandBase
+    {
+        public override bool IsCommand => true;
+    }
+    public class ConVar : ConCommandBase
+    {
+        public override bool IsCommand => false;
+
+        public delegate void OnConvarChangeDelegate(ConVar self, CVValue old, CVValue now);
+        public event ChangeCallback? OnChange;
+
+        public string DefaultValue { get; set; }
+
+        private double? minimum = null;
+        private double? maximum = null;
+        private CVValue value = new();
+
+        public double? Minimum {
+            get => minimum;
+            set {
+                minimum = value;
+                Clamp();
+            }
+        }
+
+        public double? Maximum {
+            get => maximum;
+            set {
+                maximum = value;
+                Clamp();
+            }
+        }
+
+        public ConVar(string name, string defaultValue, ConsoleFlags flags, string helpString) : base(name, helpString, flags) {
+            DefaultValue = defaultValue;
+            Update(DefaultValue);
+        }
+
+        public ConVar(string name, string defaultValue, ConsoleFlags flags) : this(name, defaultValue, flags, "") { }
+        public ConVar(string name, string defaultValue, ConsoleFlags flags, string helpString, double? min = null, double? max = null) : this(name, defaultValue, flags, helpString) {
+            Minimum = min;
+            Maximum = max;
+        }
+
+        private void Update(string input) {
+            var old = value;
+            var changed = CVValue.Update(ref value, input, Minimum, Maximum);
+            if (changed)
+                OnChange?.Invoke(this, old, value);
+        }
+
+        private void Clamp() {
+            var old = value;
+            var changed = CVValue.Clamp(ref value, Minimum, Maximum);
+            if (changed)
+                OnChange?.Invoke(this, old, value);
+        }
+
+        public void Revert() {
+            Update(DefaultValue);
+        }
+
+        public double GetDouble() => value.AsDouble ?? 0;
+        public int GetInt() => value.AsInt ?? 0;
+        public string GetString() => value.String ?? "";
+        public bool GetBool() => (value.AsDouble ?? 0) >= 1;
+
+        public void SetValue(string str) => Update(str);
+        public void SetValue(int i) => Update(Convert.ToString(i, CultureInfo.InvariantCulture));
+        public void SetValue(double d) => Update(Convert.ToString(d, CultureInfo.InvariantCulture));
+        public void SetValue(bool b) => Update(Convert.ToString(b, CultureInfo.InvariantCulture));
+
+        public static ConVar Register(
+            string name,
+            string defaultValue,
+            ConsoleFlags flags,
+            string helpString,
+            double? min = null,
+            double? max = null
+        ) {
+            if (IsRegistered(name)) {
+                var t = lookup[name];
+                if (t is ConVar cv)
+                    return cv;
+
+                throw new Exception($"ConCommandBase '{name}' already existed and was not a ConVar");
+            }
+
+            ConVar? cmd = (ConVar?)Activator.CreateInstance(typeof(ConVar), [name, defaultValue, flags, helpString, min, max]);
+            if (cmd == null) throw new Exception("ConVar: null?");
+
+            lookup[name] = cmd;
+
+            return cmd;
+        }
+    }
+
     // to-do
     public struct ConsoleMessage {
         public DateTime Time;
--- Nucleus/Engine/CommandLineArguments.cs
+++ Nucleus/Engine/CommandLineArguments.cs
@@ -1,45 +1,131 @@
 ﻿namespace Nucleus.Engine
 {
-    public class CommandLineArguments
+    public static class CommandLineArguments
     {
-        private CommandLineArguments() { }
+        private static Dictionary<string, object> parameters = [];
 
-        private HashSet<string> Flags = [];
-        private Dictionary<string, string> Vars = [];
-        public string[] Raw { get; private set; } = [];
+        public static bool HasParam(string parm) => parameters.TryGetValue(parm, out var _);
+        private static bool getStrBoolVal(string parm) {
+            switch (parm.ToLower()) {
+                case "t":
+                case "true":
+                case "y":
+                case "yes":
+                case "1":
+                    return true;
+                case "f":
+                case "false":
+                case "n":
+                case "no":
+                case "0":
+                default:
+                    return false;
+            }
+        }
+        public static bool IsParamTrue(string parm) => parameters.TryGetValue(parm, out var p) && (
+                (p is int i && i >= 1) ||
+                (p is int d && d >= 1) ||
+                (p is string s && getStrBoolVal(s))
+            );
+        public static T GetParam<T>(string parm, T def)
+            => parameters.TryGetValue(parm, out object? obj)
+                ? (obj is T ? (T)obj : def)
+                : def;
+        public static void SetParam<T>(string parm, T val) {
+            object? oVal = val;
+            if (oVal == null) parameters.Remove(parm);
+            else parameters[parm] = oVal;
+        }
 
-        public bool IsFlagSet(string flag) => Flags.Contains(flag);
-        public bool IsVarSet(string var) => Vars.ContainsKey(var);
-        public bool TryGetVar(string var, out string? output) => Vars.TryGetValue(var, out output);
-        public string TryGetVar(string var, string def) => Vars.TryGetValue(var, out var ret) ? ret : def;
-
-        public static CommandLineArguments FromArgs(string[] args)
-        {
-            CommandLineArguments ret = new CommandLineArguments();
-            ret.Raw = args;
-
-            string? key = null;
-            foreach (var part in args)
-            {
-                if (key != null)
-                {
-                    ret.Vars[key] = part;
-                    key = null;
-                }
-                else
-                {
-                    if (part.StartsWith("-"))
-                    {
-                        ret.Flags.Add(part.Substring(1));
-                    }
-                    else
-                    {
-                        key = part;
-                    }
-                }
+        private static string readUntil(string str, ref int i, char stopChar) {
+            string build = "";
+            while (i < str.Length) {
+                char c = str[i];
+                if (c == stopChar)
+                    return build;
+                build += c;
+                i++;
             }
 
-            return ret;
+            return build;
+        }
+
+        private static string readValue(string str, ref int i) {
+            string build = "";
+            bool willBeQuoted = str[i] == '"';
+
+            while (i < str.Length) {
+                char c = str[i];
+                if ((c == ' ' && !willBeQuoted) || (c == '"' && willBeQuoted && str[i - 1] != '\\'))
+                    return build;
+                build += c;
+                i++;
+            }
+
+            return build;
+        }
+
+        private static void skipWhitespace(string args, ref int i) {
+            while (i < args.Length && char.IsWhiteSpace(args[i]))
+                i++;
+        }
+
+        private static object trueValueType(string input) {
+            if (int.TryParse(input, out int i))
+                return i;
+
+            if (double.TryParse(input, out double d))
+                return d;
+
+            return input;
+        }
+
+        private static bool checkIfValue(string input, int i) {
+            if (i < input.Length && input[i] != '-' && input[i] != '+')
+                return true;
+            return false;
+        }
+
+        public static void FromString(string args) {
+            for (int i = 0; i < args.Length; i++) {
+                skipWhitespace(args, ref i);
+                char c = args[i];
+                switch (c) {
+                    case '-': // Read a parameter
+                    case '+': // Read a variable
+                        bool isParm = c == '-';
+                        i++;
+                        var parm = readUntil(args, ref i, ' ');
+                        skipWhitespace(args, ref i);
+
+                        bool hasValue = checkIfValue(args, i);
+                        string value = "";
+
+                        if (hasValue)
+                            value = readValue(args, ref i);
+                        else if (isParm)
+                            value = "1";
+
+                        if (isParm) {
+                            parameters[parm] = trueValueType(value);
+                        }
+                        else {
+                            // no logic right now to handle variables
+                            // to do as i figure out how to fit that in
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void FromArgs(string[] args) {
+            for (int i = 0; i < args.Length; i++)
+                args[i] = args[i].IndexOf(' ') > -1 ? ('"' + args[i] + '"') : args[i];
+
+            FromString(string.Join(' ', args));
         }
     }
 }
