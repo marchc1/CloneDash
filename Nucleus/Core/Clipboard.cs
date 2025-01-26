92a53e16da32d8b62c1ba7d4a7ee0d32523022b8
march <106459595+marchc1@users.noreply.github.com>
2025-01-25 20:41:38 -0800

    Add clipboard support

---
+++ Nucleus/Core/Clipboard.cs
@@ -0,0 +1,18 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus
+{
+	public static class Clipboard
+	{
+		public static string Text {
+			get => Raylib.GetClipboardText_();
+			set => Raylib.SetClipboardText(value);
+		}
+	}
+}
