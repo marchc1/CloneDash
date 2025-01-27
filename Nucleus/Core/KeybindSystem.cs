e7372b3a377a5b835791862ca0a0578fe6ff1a5c
march <106459595+marchc1@users.noreply.github.com>
2025-01-26 16:27:28 -0800

    Implement new KeybindSystem from sep. branch

--- Nucleus/Core/KeybindSystem.cs
+++ Nucleus/Core/KeybindSystem.cs
@@ -1,93 +1,90 @@
 ﻿using Nucleus.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Nucleus.Core
 {
-    public class KeybindSystem {
-        internal Dictionary<KeyboardKey, List<Keybind>> FinalKeybindAssociation { get; } = [];
+	public class KeybindSystem
+	{
+		internal Dictionary<KeyboardKey, List<Keybind>> FinalKeybindAssociation { get; } = [];
 
-        public Keybind AddKeybind(List<KeyboardKey> requiredKeys, Action bind) {
-            Keybind ret = Keybind.Make(requiredKeys, bind);
+		public Keybind AddKeybind(List<KeyboardKey> requiredKeys, Action bind, bool mustBePure = false) {
+			Keybind ret = Keybind.Make(requiredKeys, bind, mustBePure);
 
-            if (!FinalKeybindAssociation.ContainsKey(ret.FinalKey))
-                FinalKeybindAssociation[ret.FinalKey] = [];
-            FinalKeybindAssociation[ret.FinalKey].Add(ret);
+			if (!FinalKeybindAssociation.ContainsKey(ret.FinalKey))
+				FinalKeybindAssociation[ret.FinalKey] = [];
+			FinalKeybindAssociation[ret.FinalKey].Add(ret);
 
-            return ret;
-        }
+			return ret;
+		}
 
-        public bool TestKeybinds(KeyboardState state) {
-            bool ranKeybinds = false;
+		public bool TestKeybinds(KeyboardState state) {
+			bool ranKeybinds = false;
 
-            foreach (var keybindFinal in FinalKeybindAssociation) {
-                if (!state.KeyPressed(keybindFinal.Key))
-                    continue;
-                foreach (var keybind in keybindFinal.Value) {
-                    //Need to check the state of every key.
-                    bool Passed = true;
-                    foreach (var k in keybind.RequiredKeys) {
-                        if (!state.KeyDown(k)) {
-                            Passed = false;
-                            break; //move onto other keybinds
-                        }
-                    }
+			foreach (var keybindFinal in FinalKeybindAssociation) {
+				if (!state.KeyPressed(keybindFinal.Key))
+					continue;
 
-                    if (Passed) {
-                        //Console.WriteLine($"Likely key bind {keybind.NiceKeybindString}, checking for any disturbances...");
-                        //ok, every other key matches. need to make sure theres not a single key currently down that might interfere with this keybind
-                        bool FinalCheck = true;
-                        foreach (var keyDown in state.KeysPressed) {
-                            bool isThisAKey = false;
-                            foreach (var keyRequired in keybind.RequiredKeys) {
-                                if (keyRequired.Key == keyDown) {
-                                    isThisAKey = true;
-                                }
-                            }
+				keybindFinal.Value.Sort((x, y) => y.Complexity.CompareTo(x.Complexity));
+				foreach (var keybindTest in keybindFinal.Value) {
+					if (keybindTest.Test(state)) {
+						ranKeybinds = true;
+						keybindTest.Bind?.Invoke();
+						return true;
+					}
+				}
+			}
 
-                            //final just in case
-                            if (keyDown == keybind.FinalKey.Key)
-                                isThisAKey = true;
+			return ranKeybinds;
+		}
+	}
 
-                            if (isThisAKey == false) {
-                                FinalCheck = false;
-                                break;
-                            }
-                        }
+	public class Keybind
+	{
+		public List<KeyboardKey> RequiredKeys;
+		public KeyboardKey FinalKey;
+		public Action Bind;
+		public string NiceKeybindString;
+		public bool MustBePure = false;
+		public int Complexity => RequiredKeys.Count;
+		internal Keybind() { }
 
-                        if (FinalCheck) {
-                            Action bind;
-                            keybind.Bind?.Invoke();
-                            ranKeybinds = true;
-                        }
-                    }
-                }
-            }
+		public bool Test(KeyboardState state) {
+			foreach (var key in RequiredKeys) {
+				if (!state.KeyDown(key))
+					return false;
+			}
 
-            return ranKeybinds;
-        }
-    }
+			if (MustBePure) {
+				foreach (var key in state.KeysHeld) {
+					KeyboardKey k = KeyboardLayout.USA.FromInt(key);
+					if (!RequiredKeys.Contains(k) && k != FinalKey) {
+						return false;
+					}
+				}
+			}
 
-    public class Keybind {
-        public List<KeyboardKey> RequiredKeys;
-        public KeyboardKey FinalKey;
-        public Action Bind;
-        public string NiceKeybindString;
+			return state.KeyPressed(FinalKey);
+		}
 
-        internal Keybind() { }
+		public static Keybind Make(List<KeyboardKey> requiredKeys, Action bind, bool mustBePure) {
+			Keybind ret = new Keybind();
 
-        public static Keybind Make(List<KeyboardKey> requiredKeys, Action bind) {
-            Keybind ret = new Keybind();
+			ret.RequiredKeys = requiredKeys;
+			ret.FinalKey = requiredKeys.Last();
+			ret.Bind = bind;
+			ret.MustBePure = mustBePure;
 
-            ret.RequiredKeys = requiredKeys;
-            ret.FinalKey = requiredKeys.Last();
-            ret.Bind = bind;
+			List<string> keyNames = [];
+			foreach (KeyboardKey key in requiredKeys) {
+				keyNames.Add(KeyboardLayout.USA.FromInt(key.Key).Name);
+			}
+			ret.NiceKeybindString = string.Join(" + ", keyNames);
 
-            List<string> keyNames = [];
-            foreach (KeyboardKey key in requiredKeys) {
-                keyNames.Add(KeyboardLayout.USA.FromInt(key.Key).Name);
-            }
-            ret.NiceKeybindString = string.Join(" + ", keyNames);
-
-            return ret;
-        }        
-    }
+			return ret;
+		}
+	}
 }
