04834e276245bb19cb73b345d0e4c9bdbfe590fe
march <106459595+marchc1@users.noreply.github.com>
2025-01-29 17:48:18 -0800

    Add CharacterDescriptor & Mod

---
+++ CloneDash/Modding/Descriptors/CharacterDescriptor.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace CloneDash.Modding.Descriptors
+{
+	public class CharacterDescriptor : CloneDashDescriptor
+	{
+		[JsonIgnore] private static readonly string[] Attacks = ["Hit1", "Hit2", "Hit3"];
+
+		public string[] Animation_AirAttacks_Failed = ["Jump"];
+		public string[] Animation_GroundAttacks_Failed = ["Punch"];
+
+		public string[]? Animation_Attacks = null;
+		public string[]? Animation_AirAttacks = null;
+		public string[]? Animation_GroundAttacks = Attacks;
+
+		public string[] GetAirAttacks() =>
+			Animation_AirAttacks == null ? Animation_Attacks ?? throw new Exception("No air attacks!") : Animation_AirAttacks;
+		public string[] GetGroundAttacks() =>
+			Animation_GroundAttacks == null ? Animation_Attacks ?? throw new Exception("No ground attacks!") : Animation_GroundAttacks;
+
+		public string Animation_Holding = "Holding";
+
+		public static CharacterDescriptor ParseFile(string filepath) => ParseFile<CharacterDescriptor>(filepath);
+	}
+}
---
+++ CloneDash/Modding/Settings/CharacterMod.cs
@@ -0,0 +1,32 @@
+﻿using CloneDash.Modding.Descriptors;
+using Nucleus;
+using Nucleus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneDash.Modding.Settings
+{
+	[Nucleus.MarkForStaticConstruction]
+	public static class CharacterMod
+	{
+		public static ConVar clonedash_character = ConVar.Register("clonedash_character", "", ConsoleFlags.Saved, "Your character.");
+
+		static CharacterMod() {
+			Filesystem.AddPath("chars", Filesystem.Resolve("game") + "assets/chars/");
+		}
+
+		public static (string Name, CharacterDescriptor Descriptor) GetCharacterData() {
+			string name = clonedash_character.GetString();
+			if (string.IsNullOrWhiteSpace(name)) {
+				throw new Exception("Cannot load character; clonedash_character convar empty");
+			}
+
+			CharacterDescriptor descriptor = CharacterDescriptor.ParseFile(Filesystem.Resolve($"{name}.cdd", "chars"));
+			return (name, descriptor);
+		}
+	}
+}
+
