8bad832162f912bf455307fdaf79a1195f6e6a3e
march <106459595+marchc1@users.noreply.github.com>
2025-01-29 17:48:01 -0800

    Create Modding core

---
+++ CloneDash/Modding/Descriptors/CloneDashDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Nucleus.Platform;
+
+namespace CloneDash.Modding.Descriptors
+{
+	[Nucleus.MarkForStaticConstruction]
+	public abstract class CloneDashDescriptor
+	{
+		public int Version;
+		public static T ParseFile<T>(string filepath) where T : CloneDashDescriptor => JsonConvert.DeserializeObject<T>(File.ReadAllText(filepath)) ?? throw new Exception("Could not parse the file.");
+	
+		static CloneDashDescriptor() {
+			FileAssoc.Register(".cdd", "CloneDash.Descriptor", "Clone Dash Descriptor File");
+		}
+	}
+}
