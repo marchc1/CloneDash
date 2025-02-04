b03819f01a24a16f8ca2f113a8a7578c7be9b956
march <106459595+marchc1@users.noreply.github.com>
2025-02-04 11:26:04 -0800

    Move trig methods

--- Nucleus/Nucleus.Math/Constants.cs
+++ Nucleus/Nucleus.Math/Constants.cs
@@ -2,9 +2,5 @@
 {
     public static partial class NMath
     {
-        /// <summary>
-        /// (degrees) / DEG2RAD = (radians)
-        /// </summary>
-        public const float DEG2RAD = 180f / 3.141592653589793f;
     }
 }
---
+++ Nucleus/Nucleus.Math/Trig.cs
@@ -0,0 +1,14 @@
+﻿namespace Nucleus
+{
+	public static partial class NMath
+	{
+		/// <summary>
+		/// (degrees) / DEG2RAD = (radians)
+		/// </summary>
+		public const float DEG2RAD = 180f / 3.141592653589793f;
+		public const float RAD2DEG = 3.141592653589793f / 180f;
+
+		public static float ToRadians(float degrees) => degrees * RAD2DEG;
+		public static float ToDegrees(float radians) => radians * DEG2RAD;
+	}
+}
