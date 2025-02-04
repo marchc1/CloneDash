32e34aa6252749377038f63af202abf13865f494
march <106459595+marchc1@users.noreply.github.com>
2025-02-03 16:20:41 -0800

    Array IndexOf with predicate

---
+++ Nucleus/Util/ArrayTools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Util
+{
+	public static unsafe partial class Util
+	{
+		public static int IndexOf<T>(this IList<T> items, Predicate<T> search) {
+			for (int i = 0; i < items.Count; i++) {
+				if (search(items[i]))
+					return i;
+			}
+
+			return -1;
+		}
+	}
+}
