4b889f09783a9d945b62aa26f716cd49c1500064
march <106459595+marchc1@users.noreply.github.com>
2025-02-06 00:09:33 -0800

    CreateOptionPanel

--- Nucleus.ModelEditor/UI/EditorDialogs.cs
+++ Nucleus.ModelEditor/UI/EditorDialogs.cs
@@ -20,14 +20,34 @@ namespace Nucleus.ModelEditor.UI
 
 			return w;
 		}
-		private static void SetupDescription(Window dialog, string text) {
+
+		public static (Panel Panel, Checkbox Checkbox) CreateOptionPanel(Window dialog, bool isChecked, string label) {
+			Panel p = dialog.Add<Panel>();
+			p.Dock = Dock.Top;
+			p.Size = new(32);
+			p.DrawPanelBackground = false;
+
+			Checkbox c = p.Add<Checkbox>();
+			c.Dock = Dock.Left;
+			c.Size = new(28);
+			c.Checked = isChecked;
+
+			var l = p.Add<Label>();
+			l.AutoSize = true;
+			l.Text = label;
+			l.Dock = Dock.Left;
+
+			return (p, c);
+		}
+
+		public static void SetupDescription(Window dialog, string text) {
 			var lbl = dialog.Add<Label>();
 			lbl.Text = text;
 			lbl.AutoSize = true;
 			lbl.Dock = Dock.Top;
 			lbl.DockMargin = Types.RectangleF.TLRB(4);
 		}
-		private static void SetupOKCancelButtons(Window dialog, bool preferOK, Action? confirmed, Action? denied) {
+		public static void SetupOKCancelButtons(Window dialog, bool preferOK, Action? confirmed, Action? denied) {
 			var buttons = dialog.Add<CenteredObjectsPanel>();
 			buttons.Dock = Dock.Bottom;
 			buttons.Size = new(0, 42);
