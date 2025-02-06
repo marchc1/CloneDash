e9a3cc534363ef05962f1efce6b246a9ba60cd66
march <106459595+marchc1@users.noreply.github.com>
2025-02-05 17:54:00 -0800

    uint.RoundUpToPowerOf2

---
+++ Nucleus/Util/NumTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Util
+{
+	public static unsafe partial class Util
+	{
+		public static uint RoundUpToPowerOf2(this uint x) {
+			return BitOperations.RoundUpToPowerOf2(x);
+		}
+	}
+}
