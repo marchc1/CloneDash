1d994bbec5680cb5202d14032dcdf6a5035eee39
march <106459595+marchc1@users.noreply.github.com>
2025-02-06 14:29:08 -0800

    Add editor list types

---
+++ Nucleus.ModelEditor/EditorTypes/SkinsList.cs
@@ -0,0 +1,18 @@
+﻿using Nucleus.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.ModelEditor
+{
+	public class SkinsList() : EditorList<EditorSkin>("skins", "skins")
+	{
+		public override void BuildOperators(Panel buttons, PreUIDeterminations determinations) {
+			PropertiesPanel.NewMenu(buttons, [
+				new("Skin", () => PropertiesPanel.NewSkinDialog(ModelEditor.Active.File, Model))
+			]);
+		}
+	}
+}
