ce7e86006b5cd5e2f4fc00c94c5b1969ece40944
march <106459595+marchc1@users.noreply.github.com>
2025-02-06 14:27:15 -0800

    Implement skins

---
+++ Nucleus.ModelEditor/EditorTypes/EditorSkin.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Nucleus.ModelEditor
+{
+	public class EditorSkin : IEditorType
+	{
+		public EditorModel Model { get; set; }
+		public string Name { get; set; } = "";
+
+		[JsonIgnore] public string SingleName => "skin";
+
+		[JsonIgnore] public string PluralName => "skins";
+
+		[JsonIgnore] public bool Hovered { get; set; }
+		[JsonIgnore] public bool Selected { get; set; }
+		[JsonIgnore] public bool Hidden { get; set; }
+
+		public bool CanHide() => true;
+
+		public bool GetVisible() => Model.ActiveSkin == this;
+		public void OnShown() => ModelEditor.Active.File.SetActiveSkin(this);
+		public void OnHidden() => ModelEditor.Active.File.UnsetActiveSkin(this);
+	}
+}
