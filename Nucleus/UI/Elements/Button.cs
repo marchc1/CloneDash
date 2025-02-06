c3a9320406d76cc044dd1c0f254bf0d711025ceb
march <106459595+marchc1@users.noreply.github.com>
2025-02-06 00:08:59 -0800

    Add support for circular buttons

--- Nucleus/UI/Elements/Button.cs
+++ Nucleus/UI/Elements/Button.cs
@@ -5,20 +5,20 @@ using Raylib_cs;
 
 namespace Nucleus.UI
 {
-    public class Button : Label
-    {
-        protected override void Initialize() {
-            base.Initialize();
-        }
-        protected override void OnThink(FrameState frameState) {
-            if (Hovered)
-                EngineCore.SetMouseCursor(MouseCursor.MOUSE_CURSOR_POINTING_HAND);
-            if (TriggeredWhenEnterPressed && frameState.KeyboardState.KeyPressed(KeyboardLayout.USA.Enter)) {
-                MouseReleaseOccur(frameState, Types.MouseButton.MouseLeft, true);
-            }
-        }
+	public class Button : Label
+	{
+		protected override void Initialize() {
+			base.Initialize();
+		}
+		protected override void OnThink(FrameState frameState) {
+			if (Hovered)
+				EngineCore.SetMouseCursor(MouseCursor.MOUSE_CURSOR_POINTING_HAND);
+			if (TriggeredWhenEnterPressed && frameState.KeyboardState.KeyPressed(KeyboardLayout.USA.Enter)) {
+				MouseReleaseOccur(frameState, Types.MouseButton.MouseLeft, true);
+			}
+		}
 
-        public bool TriggeredWhenEnterPressed {
+		public bool TriggeredWhenEnterPressed {
 			get => __triggeredWhenEnterPressed;
 			set {
 				__triggeredWhenEnterPressed = value;
@@ -30,7 +30,7 @@ namespace Nucleus.UI
 		private bool __pulsing = false;
 		private DateTime startPulse;
 		public float PulseTime => (float)(DateTime.UtcNow - startPulse).TotalSeconds;
-        public bool Pulsing {
+		public bool Pulsing {
 			get => __pulsing;
 			set {
 				__pulsing = value;
@@ -38,24 +38,35 @@ namespace Nucleus.UI
 			}
 		}
 
-        public override void Paint(float width, float height) {
-            var backpre = BackgroundColor;
-            var forepre = ForegroundColor;
-            if (TriggeredWhenEnterPressed || Pulsing) {
-                backpre = backpre.Adjust(0, 0, 1 + (Math.Sin(PulseTime * 6) * 1.9));
-				forepre = forepre.Adjust(0, 0, 1 + (Math.Sin(PulseTime * 6) * 0.1f));
-            }
-            var back = MixColorBasedOnMouseState(this, backpre, new(0, 0.8f, 2.5f, 1f), new(0, 1.2f, 0.6f, 1f));
-            var fore = MixColorBasedOnMouseState(this, forepre, new(0, 0.8f, 1.8f, 1f), new(0, 1.2f, 0.6f, 1f));
+		public bool DrawAsCircle { get; set; } = false;
 
-            Graphics2D.SetDrawColor(back);
-            Graphics2D.DrawRectangle(0, 0, width, height);
-            ImageDrawing();
-            if (BorderSize > 0) {
-                Graphics2D.SetDrawColor(fore);
-                Graphics2D.DrawRectangleOutline(0, 0, width, height, BorderSize);
-            }
-            base.Paint(width, height);
-        }
-    }
+		public override void Paint(float width, float height) {
+			var backpre = BackgroundColor;
+			var forepre = ForegroundColor;
+			if (TriggeredWhenEnterPressed || Pulsing) {
+				backpre = backpre.Adjust(0, 0, 1 + (Math.Sin(PulseTime * 6) * 1.9));
+				forepre = forepre.Adjust(0, 0, 1 + (Math.Sin(PulseTime * 6) * 0.1f));
+			}
+			var back = MixColorBasedOnMouseState(this, backpre, new(0, 0.8f, 2.5f, 1f), new(0, 1.2f, 0.6f, 1f));
+			var fore = MixColorBasedOnMouseState(this, forepre, new(0, 0.8f, 1.8f, 1f), new(0, 1.2f, 0.6f, 1f));
+
+			Graphics2D.SetDrawColor(back);
+			var whd2 = new Vector2F(width / 2, width / 2);
+			var whd3 = new Vector2F(width / 3, width / 3);
+			if (DrawAsCircle)
+				Graphics2D.DrawCircle(whd2, whd3);
+			else
+				Graphics2D.DrawRectangle(0, 0, width, height);
+
+			ImageDrawing();
+			if (BorderSize > 0) {
+				Graphics2D.SetDrawColor(fore); 
+				if (DrawAsCircle)
+					Graphics2D.DrawCircleLines(whd2, whd3);
+				else
+					Graphics2D.DrawRectangleOutline(0, 0, width, height, BorderSize);
+			}
+			base.Paint(width, height);
+		}
+	}
 }
