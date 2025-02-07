4cbf34761413215450a566994ea4244e0d5fbd75
march <106459595+marchc1@users.noreply.github.com>
2025-02-06 22:01:00 -0800

    Separate setup vs pose
    This makes animation mode work in the future

--- Nucleus/Models/Types/PoseableObject.cs
+++ Nucleus/Models/Types/PoseableObject.cs
@@ -21,20 +21,20 @@ namespace Nucleus.Models
 
 		[JsonIgnore] protected Vector2F pos = Vector2F.Zero, scale = Vector2F.One, shear = Vector2F.Zero;
 		[JsonIgnore] protected float rot = 0;
-		[JsonIgnore] protected TransformMode transformMode = TransformMode.Normal;
+		[JsonIgnore] protected TransformMode? transformMode;
 
-		public Vector2F SetupPosition { get => setupPos; set { setupPos = value; ResetToSetupPos(); } }
-		public float SetupRotation { get => setupRot; set { setupRot = value; ResetToSetupPos(); } }
-		public Vector2F SetupScale { get => setupScale; set { setupScale = value; ResetToSetupPos(); } }
-		public Vector2F SetupShear { get => setupShear; set { setupShear = value; ResetToSetupPos(); } }
-		public TransformMode SetupTransformMode { get => setupTransformMode; set { setupTransformMode = value; ResetToSetupPos(); } }
+		public Vector2F SetupPosition { get => setupPos; set { setupPos = value; ResetToSetupPose(); } }
+		public float SetupRotation { get => setupRot; set { setupRot = value; ResetToSetupPose(); } }
+		public Vector2F SetupScale { get => setupScale; set { setupScale = value; ResetToSetupPose(); } }
+		public Vector2F SetupShear { get => setupShear; set { setupShear = value; ResetToSetupPose(); } }
+		public TransformMode SetupTransformMode { get => setupTransformMode; set { setupTransformMode = value; ResetToSetupPose(); } }
 
-		[JsonIgnore] public float SetupPositionX { get => setupPos.X; set { setupPos.X = value; ResetToSetupPos(); } }
-		[JsonIgnore] public float SetupPositionY { get => setupPos.Y; set { setupPos.Y = value; ResetToSetupPos(); } }
-		[JsonIgnore] public float SetupScaleX { get => setupScale.X; set { setupScale.X = value; ResetToSetupPos(); } }
-		[JsonIgnore] public float SetupScaleY { get => setupScale.Y; set { setupScale.Y = value; ResetToSetupPos(); } }
-		[JsonIgnore] public float SetupShearX { get => setupShear.X; set { setupShear.X = value; ResetToSetupPos(); } }
-		[JsonIgnore] public float SetupShearY { get => setupShear.Y; set { setupShear.Y = value; ResetToSetupPos(); } }
+		[JsonIgnore] public float SetupPositionX { get => setupPos.X; set { setupPos.X = value; ResetToSetupPose(); } }
+		[JsonIgnore] public float SetupPositionY { get => setupPos.Y; set { setupPos.Y = value; ResetToSetupPose(); } }
+		[JsonIgnore] public float SetupScaleX { get => setupScale.X; set { setupScale.X = value; ResetToSetupPose(); } }
+		[JsonIgnore] public float SetupScaleY { get => setupScale.Y; set { setupScale.Y = value; ResetToSetupPose(); } }
+		[JsonIgnore] public float SetupShearX { get => setupShear.X; set { setupShear.X = value; ResetToSetupPose(); } }
+		[JsonIgnore] public float SetupShearY { get => setupShear.Y; set { setupShear.Y = value; ResetToSetupPose(); } }
 
 		/// <summary>
 		/// If false, code using PoseableObjects should not respect pose values and only respect setup values.
@@ -45,7 +45,7 @@ namespace Nucleus.Models
 		[JsonIgnore] public float Rotation { get => rot; set { rot = value; InvalidateTransform(); } }
 		[JsonIgnore] public Vector2F Scale { get => scale; set { scale = value; InvalidateTransform(); } }
 		[JsonIgnore] public Vector2F Shear { get => shear; set { shear = value; InvalidateTransform(); } }
-		[JsonIgnore] public TransformMode TransformMode { get => transformMode; set { transformMode = value; InvalidateTransform(); } }
+		[JsonIgnore] public TransformMode TransformMode { get => transformMode ?? setupTransformMode; set { transformMode = value; InvalidateTransform(); } }
 
 		[JsonIgnore] public float PositionX { get => pos.X; set { pos.X = value; InvalidateTransform(); } }
 		[JsonIgnore] public float PositionY { get => pos.Y; set { pos.Y = value; InvalidateTransform(); } }
@@ -61,11 +61,13 @@ namespace Nucleus.Models
 				if (!WorldTransformValid) {
 					var parent = GetParent();
 					worldTransform = Transformation.CalculateWorldTransformation(
-						Position,
-						Rotation,
-						Scale,
-						Shear,
-						TransformMode, parent == null ? null : parent.WorldTransform);
+						SetupPosition + Position,
+						SetupRotation + Rotation,
+						SetupScale * Scale,
+						SetupShear + Shear,
+						transformMode ?? setupTransformMode, 
+						parent == null ? null : parent.WorldTransform
+					);
 					WorldTransformValid = true;
 				}
 
@@ -88,12 +90,12 @@ namespace Nucleus.Models
 		/// <returns>The objects children.</returns>
 		public abstract IEnumerable<PoseableObject>? GetChildren();
 
-		public void ResetToSetupPos() {
-			pos = SetupPosition;
-			rot = SetupRotation;
-			scale = SetupScale;
-			shear = SetupShear;
-			transformMode = SetupTransformMode;
+		public void ResetToSetupPose() {
+			pos = Vector2F.Zero;
+			rot = 0;
+			scale = Vector2F.One;
+			shear = Vector2F.Zero;
+			transformMode = null;
 			InvalidateTransform();
 		}
 
