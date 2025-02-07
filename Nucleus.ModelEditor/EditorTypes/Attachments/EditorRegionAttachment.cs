7a28421cd350c9176853f148a9fb6fbed9ee808f
march <106459595+marchc1@users.noreply.github.com>
2025-02-06 22:07:07 -0800

    Implement region attachment (WIP)

--- Nucleus.ModelEditor/EditorTypes/Attachments/EditorRegionAttachment.cs
+++ Nucleus.ModelEditor/EditorTypes/Attachments/EditorRegionAttachment.cs
@@ -1,4 +1,6 @@
-﻿using Nucleus.Types;
+﻿using Nucleus.ManagedMemory;
+using Nucleus.Models;
+using Nucleus.Types;
 using Raylib_cs;
 using System;
 using System.Collections.Generic;
@@ -10,16 +12,80 @@ namespace Nucleus.ModelEditor
 {
 	public class EditorRegionAttachment : EditorAttachment
 	{
-		public string Path { get; set; }
-		public Color Color { get; set; }
-		public Vector2F[] Position { get; set; } = new Vector2F[4];
-		public Vector2F[] TexCoords { get; set; } = new Vector2F[4];
-		public Vector2F Size { get; set; }
-		public Vector2F Scale { get; set; }
-		public Vector2F[] VertexOffset { get; set; } = new Vector2F[4];
-		public Vector2F RegionSize { get; set; }
-		public Vector2F RegionOffset { get; set; }
-		public Vector2F RegionOriginalSize { get; set; }
+		public override string SingleName => "region";
+		public override string PluralName => "regions";
+		public override string EditorIcon => "models/region.png";
+
+		public string GetPath() => Path ?? $"<{Name}>";
+		public string? Path { get; set; } = null;
+
+		private Vector2F pos, scale = new(1, 1);
+
+		public Vector2F Position { get => pos; set => pos = value; }
 		public float Rotation { get; set; }
+		public Vector2F Scale { get => scale; set => scale = value; }
+
+		public override bool CanTranslate() => true;
+		public override bool CanRotate() => true;
+		public override bool CanScale() => true;
+		public override bool CanShear() => false;
+		public override bool CanHide() => true;
+
+		public override float GetTranslationX() => Position.X;
+		public override float GetTranslationY() => Position.Y;
+		public override float GetRotation() => Rotation;
+		public override float GetScaleX() => Scale.X;
+		public override float GetScaleY() => Scale.Y;
+
+		public override void EditTranslationX(float value) => pos.X = value;
+		public override void EditTranslationY(float value) => pos.Y = value;
+		public override void EditRotation(float value) => Rotation = value;
+		public override void EditScaleX(float value) => scale.X = value;
+		public override void EditScaleY(float value) => scale.Y = value;
+
+		public Transformation WorldTransform;
+
+		public Color Color { get; set; } = Color.WHITE;
+
+		public override void Render() {
+			var model = Slot.Bone.Model;
+
+			ModelImage? image = model.ResolveImage(Path);
+			if (image == null || Path == null) throw new Exception(":(");
+			// todo ^^ missing texture (prob just purple-black checkerboard)
+
+			WorldTransform = Transformation.CalculateWorldTransformation(pos, Rotation, scale, Vector2F.Zero, TransformMode.Normal, Slot.Bone.WorldTransform);
+			var region = model.Images.TextureAtlas.GetTextureRegion(image.Name) ?? throw new Exception("No region!");
+			float width = region.H, height = region.W;
+			float widthDiv2 = width / 2, heightDiv2 = height / 2;
+			Texture tex = model.Images.TextureAtlas.Texture;
+
+			Vector2F TL = WorldTransform.LocalToWorld(-heightDiv2, -widthDiv2);
+			Vector2F TR = WorldTransform.LocalToWorld(heightDiv2, -widthDiv2);
+			Vector2F BR = WorldTransform.LocalToWorld(heightDiv2, widthDiv2);
+			Vector2F BL = WorldTransform.LocalToWorld(-heightDiv2, widthDiv2);
+
+			Rlgl.Begin(DrawMode.TRIANGLES);
+			Rlgl.SetTexture(((Texture2D)tex).Id);
+
+			Rlgl.Color4ub(255, 255, 255, 255);
+
+			float uStart, uEnd, vStart, vEnd;
+			uStart = (float)region.X / (float)tex.Width;
+			uEnd = uStart + ((float)region.W / (float)tex.Width);
+
+			vStart = ((float)region.Y / (float)tex.Height);
+			vEnd = vStart + ((float)region.H / (float)tex.Height);
+
+			Rlgl.TexCoord2f(uStart, vEnd); Rlgl.Vertex3f(BL.X, BL.Y, 0);
+			Rlgl.TexCoord2f(uEnd, vStart); Rlgl.Vertex3f(TR.X, TR.Y, 0);
+			Rlgl.TexCoord2f(uStart, vStart); Rlgl.Vertex3f(TL.X, TL.Y, 0);
+
+			Rlgl.TexCoord2f(uEnd, vEnd); Rlgl.Vertex3f(BR.X, BR.Y, 0);
+			Rlgl.TexCoord2f(uEnd, vStart); Rlgl.Vertex3f(TR.X, TR.Y, 0);
+			Rlgl.TexCoord2f(uStart, vEnd); Rlgl.Vertex3f(BL.X, BL.Y, 0);
+
+			Rlgl.End();
+		}
 	}
 }
