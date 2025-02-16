740007ac05fc70df0daea9c3043a4597491c309b
march <106459595+marchc1@users.noreply.github.com>
2025-02-16 00:38:25 -0800

    Remove copy-pasted stuff

--- Nucleus.ModelEditor/UI/VertexWeightOperator.cs
+++ Nucleus.ModelEditor/UI/VertexWeightOperator.cs
@@ -18,81 +18,35 @@ namespace Nucleus.ModelEditor
 	public class VertexWeightOperator : DefaultOperator
 	{
 		public override void GizmoRender(EditorPanel editorPanel, IEditorType target) {
-
+			return; // Placeholder
 		}
 
-		private EditorBone? parentBone;
-		private EditorBone? bone;
-		Vector2F gridDragLast;
-
 		public override bool GizmoStartDragging(EditorPanel editorPanel, Vector2F mouseScreenStart, IEditorType? currentSelection, IEditorType? clicked) {
-			if (parentBone == null) return false;
-			MakeBone();
-			var bonePos = parentBone.WorldTransform.WorldToLocal(editorPanel.ScreenToGrid(mouseScreenStart));
-			ModelEditor.Active.File.TranslateXSelected(bonePos.X);
-			ModelEditor.Active.File.TranslateYSelected(bonePos.Y);
-
-			return bone != null;
+			return false; // Placeholder
 		}
 
 		public override void GizmoDrag(EditorPanel editorPanel, Vector2F mouseScreenStart, Vector2F mouseScreenNow, IEnumerable<IEditorType> targets) {
-			if (bone == null) return;
-			var bonePos = bone.WorldTransform.LocalToWorld(0, 0);
-			var boneEnd = editorPanel.ScreenToGrid(mouseScreenNow);
-			var length = boneEnd.Distance(bonePos);
-			var delta = boneEnd - bonePos;
-			var rotation = MathF.Atan2(delta.Y, delta.X).ToDegrees();
-
-			ModelEditor.Active.File.SetBoneLength(bone, length);
-			ModelEditor.Active.File.RotateSelected(bone.WorldTransform.WorldToLocalRotation(rotation));
+			return; // Placeholder
 		}
 
 		public override void GizmoEndDragging(EditorPanel editorPanel, Vector2F mouseScreenEnd, IEditorType target) {
-			base.GizmoEndDragging(editorPanel, mouseScreenEnd, target);
+			return; // Placeholder
 		}
 
 		public override bool GizmoClicked(EditorPanel editorPanel, IEditorType? target, Vector2F mouseScreenStart) {
-			bone = null;
-
-			gridDragLast = editorPanel.ScreenToGrid(mouseScreenStart);
-
-			if (ModelEditor.Active.SelectedObjectsCount <= 0 && target != null)
-				return false;
-
-			var test = ModelEditor.Active.SelectedObjectsCount <= 0 ? ModelEditor.Active.File.Models[0].Root : ModelEditor.Active.LastSelectedObject;
-			if (test is not EditorBone boneTarget) return false;
-			parentBone = boneTarget;
-
-			ModelEditor.Active.SelectObject(parentBone);
-
-			return true;
-		}
-
-		private void MakeBone() {
-			if (parentBone == null) return;
-			if (bone != null) return;
-			bone = ModelEditor.Active.File.AddBone(parentBone.Model, parentBone).ResultOrThrow;
-			ModelEditor.Active.SelectObject(bone);
+			return false; // Placeholder
 		}
 
 		public override bool GizmoReleased(EditorPanel editorPanel, IEditorType? target, Vector2F mouseScreenStart) {
-			if (target != null)
-				return true;
-
-			MakeBone();
-			return false;
+			return false; // Placeholder
 		}
 
-		VertexWeightWindow weightWindow;
-
 		public override void Activated() {
-			base.Activated();
-			weightWindow = ModelEditor.Active.Editor.Add<VertexWeightWindow>();
+			return; // Placeholder
 		}
 
 		public override void Deactivated() {
-			base.Deactivated();
-			weightWindow?.Remove();
+			return; // Placeholder
 		}
 	}
 }
