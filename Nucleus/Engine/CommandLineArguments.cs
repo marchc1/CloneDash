30e9430dacf12e21b721e625529e918cfe49ab91
march <106459595+marchc1@users.noreply.github.com>
2025-02-18 13:11:24 -0800

    Fix several bugs in CommandLineArguments
    Also clean up formatting

--- Nucleus/Engine/CommandLineArguments.cs
+++ Nucleus/Engine/CommandLineArguments.cs
@@ -1,131 +1,146 @@
-﻿namespace Nucleus.Engine
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nucleus.Engine
 {
-    public static class CommandLineArguments
-    {
-        private static Dictionary<string, object> parameters = [];
+	public static class CommandLineArguments
+	{
+		private static Dictionary<string, object> parameters = [];
+		public static Dictionary<string, object> Params => parameters.ToDictionary();
+		public static bool HasParam(string parm) => parameters.TryGetValue(parm, out var _);
+		private static bool getStrBoolVal(string parm) {
+			switch (parm.ToLower()) {
+				case "t":
+				case "true":
+				case "y":
+				case "yes":
+				case "1":
+					return true;
+				case "f":
+				case "false":
+				case "n":
+				case "no":
+				case "0":
+				default:
+					return false;
+			}
+		}
 
-        public static bool HasParam(string parm) => parameters.TryGetValue(parm, out var _);
-        private static bool getStrBoolVal(string parm) {
-            switch (parm.ToLower()) {
-                case "t":
-                case "true":
-                case "y":
-                case "yes":
-                case "1":
-                    return true;
-                case "f":
-                case "false":
-                case "n":
-                case "no":
-                case "0":
-                default:
-                    return false;
-            }
-        }
-        public static bool IsParamTrue(string parm) => parameters.TryGetValue(parm, out var p) && (
-                (p is int i && i >= 1) ||
-                (p is int d && d >= 1) ||
-                (p is string s && getStrBoolVal(s))
-            );
-        public static T GetParam<T>(string parm, T def)
-            => parameters.TryGetValue(parm, out object? obj)
-                ? (obj is T ? (T)obj : def)
-                : def;
-        public static void SetParam<T>(string parm, T val) {
-            object? oVal = val;
-            if (oVal == null) parameters.Remove(parm);
-            else parameters[parm] = oVal;
-        }
+		public static bool IsParamTrue(string parm, bool def = false) => parameters.TryGetValue(parm, out var p) ? (
+				(p is int i && i >= 1) ||
+				(p is int d && d >= 1) ||
+				(p is string s && getStrBoolVal(s))
+			) : def;
 
-        private static string readUntil(string str, ref int i, char stopChar) {
-            string build = "";
-            while (i < str.Length) {
-                char c = str[i];
-                if (c == stopChar)
-                    return build;
-                build += c;
-                i++;
-            }
+		public static T GetParam<T>(string parm, T def)
+			=> parameters.TryGetValue(parm, out object? obj)
+				? (obj is T ? (T)obj : def)
+				: def;
 
-            return build;
-        }
+		public static bool TryGetParam<T>(string parm, [NotNullWhen(true)] out T? ret) {
+			var has = parameters.TryGetValue(parm, out object? obj);
+			if (has)
+				ret = (obj is T ? (T)obj : default);
+			else
+				ret = default;
+			return has;
+		}
 
-        private static string readValue(string str, ref int i) {
-            string build = "";
-            bool willBeQuoted = str[i] == '"';
+		public static void SetParam<T>(string parm, T val) {
+			object? oVal = val;
+			if (oVal == null) parameters.Remove(parm);
+			else parameters[parm] = oVal;
+		}
 
-            while (i < str.Length) {
-                char c = str[i];
-                if ((c == ' ' && !willBeQuoted) || (c == '"' && willBeQuoted && str[i - 1] != '\\'))
-                    return build;
-                build += c;
-                i++;
-            }
+		private static string readUntil(string str, ref int i, char stopChar) {
+			string build = "";
+			while (i < str.Length) {
+				char c = str[i];
+				if (c == stopChar)
+					return build;
+				build += c;
+				i++;
+			}
 
-            return build;
-        }
+			return build;
+		}
 
-        private static void skipWhitespace(string args, ref int i) {
-            while (i < args.Length && char.IsWhiteSpace(args[i]))
-                i++;
-        }
+		private static string readValue(string str, ref int i) {
+			string build = "";
+			bool willBeQuoted = str[i] == '"';
 
-        private static object trueValueType(string input) {
-            if (int.TryParse(input, out int i))
-                return i;
+			while (i < str.Length) {
+				char c = str[i];
+				if ((c == ' ' && !willBeQuoted) || (c == '"' && willBeQuoted && str[i - 1] != '\\'))
+					return build;
+				build += c;
+				i++;
+			}
 
-            if (double.TryParse(input, out double d))
-                return d;
+			return build;
+		}
 
-            return input;
-        }
+		private static void skipWhitespace(string args, ref int i) {
+			while (i < args.Length && char.IsWhiteSpace(args[i]))
+				i++;
+		}
 
-        private static bool checkIfValue(string input, int i) {
-            if (i < input.Length && input[i] != '-' && input[i] != '+')
-                return true;
-            return false;
-        }
+		private static object trueValueType(string input) {
+			if (int.TryParse(input, out int i))
+				return i;
 
-        public static void FromString(string args) {
-            for (int i = 0; i < args.Length; i++) {
-                skipWhitespace(args, ref i);
-                char c = args[i];
-                switch (c) {
-                    case '-': // Read a parameter
-                    case '+': // Read a variable
-                        bool isParm = c == '-';
-                        i++;
-                        var parm = readUntil(args, ref i, ' ');
-                        skipWhitespace(args, ref i);
+			if (double.TryParse(input, out double d))
+				return d;
 
-                        bool hasValue = checkIfValue(args, i);
-                        string value = "";
+			return input;
+		}
 
-                        if (hasValue)
-                            value = readValue(args, ref i);
-                        else if (isParm)
-                            value = "1";
+		private static bool checkIfValue(string input, int i) {
+			if (i < input.Length && input[i] != '-' && input[i] != '+')
+				return true;
+			return false;
+		}
 
-                        if (isParm) {
-                            parameters[parm] = trueValueType(value);
-                        }
-                        else {
-                            // no logic right now to handle variables
-                            // to do as i figure out how to fit that in
-                        }
+		public static void FromString(string args) {
+			parameters.Clear();
+			for (int i = 0; i < args.Length;) {
+				skipWhitespace(args, ref i);
+				char c = args[i];
+				switch (c) {
+					case '-': // Read a parameter
+					case '+': // Read a variable
+						bool isParm = c == '-';
+						i++;
+						var parm = readUntil(args, ref i, ' ');
+						skipWhitespace(args, ref i);
 
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+						bool hasValue = checkIfValue(args, i);
+						string value = "";
 
-        public static void FromArgs(string[] args) {
-            for (int i = 0; i < args.Length; i++)
-                args[i] = args[i].IndexOf(' ') > -1 ? ('"' + args[i] + '"') : args[i];
+						if (hasValue)
+							value = readValue(args, ref i);
+						else if (isParm)
+							value = "1";
 
-            FromString(string.Join(' ', args));
-        }
-    }
+						if (isParm) {
+							parameters[parm] = trueValueType(value);
+						}
+						else {
+							// no logic right now to handle variables
+							// to do as i figure out how to fit that in
+						}
+
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		public static void FromArgs(string[] args) {
+			for (int i = 0; i < args.Length; i++)
+				args[i] = args[i].IndexOf(' ') > -1 ? ('"' + args[i] + '"') : args[i];
+
+			FromString(string.Join(' ', args));
+		}
+	}
 }
