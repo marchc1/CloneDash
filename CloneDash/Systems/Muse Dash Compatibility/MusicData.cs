0544f2a40b6ba1e58f72d1050773645743986c12
march <106459595+marchc1@users.noreply.github.com>
2025-02-18 22:32:28 -0800

    I horribly misunderstood some fields in MusicData

--- CloneDash/Systems/Muse Dash Compatibility/MusicData.cs
+++ CloneDash/Systems/Muse Dash Compatibility/MusicData.cs
@@ -6,21 +6,26 @@
         {
             public short objId;
             public decimal tick;
-            public MusicConfigData configData;
+            public MusicConfigData? configData;
             public NoteConfigData? noteData;
             public bool isLongPressing;
             public int doubleIdx;
             public bool isDouble;
-            public bool isLongPressStart;
             public bool isLongPressEnd;
-            public bool isLongPressType;
-            public bool isAir;
             public decimal longPressPTick;
-            public int longPressCount;
             public int endIndex;
             public decimal dt;
             public int longPressNum;
             public decimal showTick;
+
+			public MusicConfigData GetConfigData() => configData ?? throw new NullReferenceException("Config data not set!");
+			public NoteConfigData GetNoteData() => noteData ?? throw new NullReferenceException("Config data not set!");
+
+			public bool isLongPressType => isLongPressStart || isLongPressing || isLongPressEnd;
+			public bool isAir => GetNoteData().pathway == 1;
+			public bool isMul => GetConfigData().length > 0m && GetNoteData().type == 8U;
+			public bool isLongPressStart => GetConfigData().length > 0m && GetNoteData().type == 3U;
+			public int longPressCount => (int)Math.Ceiling(GetConfigData().length / .1m);
         }
     }
 }
