6c793dd98b24f70332b1fe69c77560676a11617f
march <106459595+marchc1@users.noreply.github.com>
2025-04-08 22:26:50 -0700

    Tune up flexpanel

--- Nucleus/UI/Elements/FlexPanel.cs
+++ Nucleus/UI/Elements/FlexPanel.cs
@@ -6,102 +6,108 @@ namespace Nucleus.UI
 	/// FlexPanel's resizing mode. Todo: better explanations
 	/// </summary>
 	public enum FlexChildrenResizingMode
-    {
-        /// <summary>
-        /// Does not perform a resizing operation on the children of the FlexPanel.
-        /// </summary>
-        DoNotResize,
-        /// <summary>
-        /// Fits the opposite direction (opposite to FlexDirection) to match the FlexPanel's size on that axis.
-        /// </summary>
-        FitToOppositeDirection,
-        /// <summary>
-        /// Stretches the opposite direction (opposite to FlexDirection)  to match the FlexPanel's size on the opposite axis.
-        /// </summary>
-        StretchToOppositeDirection,
-        /// <summary>
-        /// Fits all elements into the FlexPanel's size on the Direction axis / Children.Count.
-        /// </summary>
-        StretchToFit
-    }
-    public class FlexPanel : Panel
-    {
-        public Directional180 Direction { get; set; } = Directional180.Horizontal;
-        public FlexChildrenResizingMode ChildrenResizingMode { get; set; } = FlexChildrenResizingMode.DoNotResize;
-        protected override void Initialize() {
-            base.Initialize();
-        }
-        public override T Add<T>(T? toAdd = null) where T : class {
-            InvalidateChildren(self: true, recursive: true);
-            return base.Add(toAdd);
-        }
-        public override void Paint(float width, float height) {
+	{
+		/// <summary>
+		/// Does not perform a resizing operation on the children of the FlexPanel.
+		/// </summary>
+		DoNotResize,
+		/// <summary>
+		/// Fits the opposite direction (opposite to FlexDirection) to match the FlexPanel's size on that axis.
+		/// </summary>
+		FitToOppositeDirection,
+		/// <summary>
+		/// Stretches the opposite direction (opposite to FlexDirection)  to match the FlexPanel's size on the opposite axis.
+		/// </summary>
+		StretchToOppositeDirection,
+		/// <summary>
+		/// Fits all elements into the FlexPanel's size on the Direction axis / Children.Count.
+		/// </summary>
+		StretchToFit
+	}
+	public class FlexPanel : Panel
+	{
+		public Directional180 Direction { get; set; } = Directional180.Horizontal;
+		public FlexChildrenResizingMode ChildrenResizingMode { get; set; } = FlexChildrenResizingMode.DoNotResize;
+		protected override void Initialize() {
+			base.Initialize();
+		}
+		public override T Add<T>(T? toAdd = null) where T : class {
+			InvalidateChildren(self: true, recursive: true);
+			return base.Add(toAdd);
+		}
+		public override void Paint(float width, float height) {
 
-        }
-        protected override void PostLayoutChildren() {
-            var childrenCount = Children.Count;
-            var ourBounds = RenderBounds;
+		}
+		protected override void PostLayoutChildren() {
+			var childrenCount = Children.Count;
+			var ourBounds = RenderBounds;
 
-            for (int i = 0; i < childrenCount; i++) {
-                Element child = Children[i];
+			for (int i = 0; i < childrenCount; i++) {
+				Element child = Children[i];
 
-                var chT = 1.0f / (childrenCount + 1.0f);
-                var chF0 = (1.0f / childrenCount) * (i + 0.0f);
-                var chF1 = chT * (i + 1.0f);
+				var chT = 1.0f / (childrenCount + 1.0f);
+				var chF0 = (1.0f / childrenCount) * (i + 0.0f);
+				var chF1 = chT * (i + 1.0f);
 
-                switch (ChildrenResizingMode) {
-                    case FlexChildrenResizingMode.DoNotResize:
-                    case FlexChildrenResizingMode.FitToOppositeDirection:
-                    case FlexChildrenResizingMode.StretchToOppositeDirection:
-                        if (Direction == Directional180.Horizontal)
-                            child.Position = new(chF1 * ourBounds.Width, ourBounds.Height / 2);
-                        else
-                            child.Position = new(ourBounds.Width / 2, chF1 * ourBounds.Height);
+				switch (ChildrenResizingMode) {
+					case FlexChildrenResizingMode.DoNotResize:
+					case FlexChildrenResizingMode.FitToOppositeDirection:
+					case FlexChildrenResizingMode.StretchToOppositeDirection:
+						if (Direction == Directional180.Horizontal)
+							child.Position = new((int)(chF1 * ourBounds.Width), ourBounds.Height / 2);
+						else
+							child.Position = new(ourBounds.Width / 2, (int)(chF1 * ourBounds.Height));
 
-                        child.Dock = Dock.None;
-                        child.Origin = Anchor.Center;
-                        break;
-                    case FlexChildrenResizingMode.StretchToFit:
-                        if (Direction == Directional180.Horizontal)
-                            child.Position = new(chF0 * ourBounds.Width, 0);
-                        else
-                            child.Position = new(0, chF0 * ourBounds.Height);
+						child.Dock = Dock.None;
+						child.Origin = Anchor.Center;
+						break;
+					case FlexChildrenResizingMode.StretchToFit:
+						if (Direction == Directional180.Horizontal)
+							child.Position = new((int)(chF0 * ourBounds.Width), 0);
+						else
+							child.Position = new(0, (int)(chF0 * ourBounds.Height));
 
-                        child.Dock = Dock.None;
-                        child.Origin = Anchor.TopLeft;
-                        break;
-                }
+						child.Dock = Dock.None;
+						child.Origin = Anchor.TopLeft;
+						break;
+				}
 
-                switch (ChildrenResizingMode) {
-                    case FlexChildrenResizingMode.DoNotResize:
-                        break;
-                    case FlexChildrenResizingMode.FitToOppositeDirection:
-                        if (Direction == Directional180.Horizontal)
-                            child.Size = new(child.RenderBounds.Width, ourBounds.Height);
-                        else
-                            child.Size = new(ourBounds.Width, child.RenderBounds.Height);
-                        break;
-                    case FlexChildrenResizingMode.StretchToOppositeDirection:
-                        if (Direction == Directional180.Horizontal)
-                            child.Size = new(ourBounds.Height, ourBounds.Height);
-                        else
-                            child.Size = new(ourBounds.Width, ourBounds.Width);
-                        break;
-                    case FlexChildrenResizingMode.StretchToFit:
-                        if (Direction == Directional180.Horizontal)
-                            child.Size = new(ourBounds.Width / childrenCount, ourBounds.Height);
-                        else
-                            child.Size = new(ourBounds.Width, ourBounds.Height / childrenCount);
+				switch (ChildrenResizingMode) {
+					case FlexChildrenResizingMode.DoNotResize:
+						break;
+					case FlexChildrenResizingMode.FitToOppositeDirection:
+						if (Direction == Directional180.Horizontal)
+							child.Size = new(child.RenderBounds.Width, ourBounds.Height);
+						else
+							child.Size = new(ourBounds.Width, child.RenderBounds.Height);
+						break;
+					case FlexChildrenResizingMode.StretchToOppositeDirection:
+						if (Direction == Directional180.Horizontal)
+							child.Size = new(ourBounds.Height, ourBounds.Height);
+						else
+							child.Size = new(ourBounds.Width, ourBounds.Width);
+						break;
+					case FlexChildrenResizingMode.StretchToFit:
+						if (Direction == Directional180.Horizontal)
+							child.Size = new((int)(ourBounds.Width / childrenCount), ourBounds.Height);
+						else
+							child.Size = new(ourBounds.Width, ourBounds.Height / childrenCount);
 
-                        break;
-                }
+						break;
+				}
 
-                if (!DockPadding.IsZero) {
-                    child.Position += new Vector2F(DockPadding.X, DockPadding.Y);
-                    child.Size -= new Vector2F(DockPadding.X, DockPadding.Y);
-                    child.Size -= new Vector2F(DockPadding.W, DockPadding.H);
-                }
-            }
-        }
-    }
+				if (!DockPadding.IsZero) {
+					child.Position += new Vector2F(DockPadding.X, DockPadding.Y);
+					child.Size -= new Vector2F(DockPadding.X, DockPadding.Y);
+					child.Size -= new Vector2F(DockPadding.W, DockPadding.H);
+				}
+
+				if (!child.DockMargin.IsZero) {
+					child.Position += new Vector2F(child.DockMargin.X, child.DockMargin.Y);
+					child.Size -= new Vector2F(child.DockMargin.X, child.DockMargin.Y);
+					child.Size -= new Vector2F(child.DockMargin.W, child.DockMargin.H);
+				}
+			}
+		}
+	}
 }
