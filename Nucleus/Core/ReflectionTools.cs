71a5c1e4f300a456e7442ae866afafd1d37b77ee
march <106459595+marchc1@users.noreply.github.com>
2025-04-08 17:10:36 -0700

    Add GetInheritorsOfAbstractType

---
+++ Nucleus/Core/ReflectionTools.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Nucleus;
+
+public static class ReflectionTools
+{
+	public static Type[] GetInheritorsOfAbstractType(this Type type)
+						=> Assembly.GetAssembly(type).GetTypes()
+						.Where(x => x.IsClass && !x.IsAbstract && x.IsSubclassOf(type))
+						.ToArray();
+}
