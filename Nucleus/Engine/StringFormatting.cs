9443ef76de7cad3a3cf79e3760479c286597541e
march <106459595+marchc1@users.noreply.github.com>
2025-04-12 13:48:34 -0700

    Update StringFormatting.cs

--- Nucleus/Engine/StringFormatting.cs
+++ Nucleus/Engine/StringFormatting.cs
@@ -10,5 +10,28 @@ namespace Nucleus
     {
         public static string FormatNumberByThousands(int n) => $"{n:n0}";
         public static string FormatNumberByThousands(double n) => n % 1 == 0 ? $"{n:n0}" : $"{n:n}";
+
+		// todo: test
+		public static string SplitAndCommas(this IEnumerable<string> stringSequence) {
+			var count = 0;
+			var builder = new StringBuilder();
+			string? lastString = null;
+			foreach(var str in stringSequence) {
+				switch (count) {
+					case 0: break;
+					case 1: builder.Append(lastString); break;
+					default: builder.Append($", {lastString}"); break;
+				}
+				lastString = str;
+				count++;
+			}
+
+			switch (count) {
+				case 0: return "";
+				case 1: return lastString;
+				case 2: return $"{builder.ToString()} and {lastString}";
+				default: return $"{builder.ToString()}, and {lastString}";
+			}
+		}
     }
 }
