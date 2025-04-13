46a131a9ca5d693998aac92fc5a9afebf6c41959
march <106459595+marchc1@users.noreply.github.com>
2025-04-12 20:00:25 -0700

    Add byte[].ToImage extension

---
+++ Nucleus/Engine/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus
+{
+	public static class Extensions
+	{
+		public static unsafe Image ToImage(this byte[] data, int width, int height, PixelFormat format, int mipmaps) {
+			var ptr = Raylib.New<byte>(data.Length);
+			for (int i = 0; i < data.Length; i++) {
+				ptr[i] = data[i];
+			}
+			var img = new Image() {
+				Data = ptr,
+				Format = format,
+				Width = width,
+				Height = height,
+				Mipmaps = mipmaps
+			};
+			return img;
+		}
+	}
+}
