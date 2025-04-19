d7db4249ee6e82788fc08af4b864fb5edf4eb3e4
march <106459595+marchc1@users.noreply.github.com>
2025-04-18 20:17:27 -0700

    Update SceneDescriptor.cs

--- CloneDash/Modding/Descriptors/SceneDescriptor.cs
+++ CloneDash/Modding/Descriptors/SceneDescriptor.cs
@@ -7,12 +7,45 @@ using System.Threading.Tasks;
 
 namespace CloneDash.Modding.Descriptors;
 
-public class SceneDescriptor : CloneDashDescriptor
+public class SceneDescriptor : CloneDashDescriptor, IDisposable
 {
+	public class SceneDescriptor_Announcer {
+		[JsonProperty("begin")] public string Begin;
+		[JsonProperty("fever")] public string Fever;
+		[JsonProperty("unpause")] public string Unpause;
+		[JsonProperty("fullcombo")] public string FullCombo;
+	}
 	public SceneDescriptor() : base(CloneDashDescriptorType.Scene, "2") { }
 
 	[JsonProperty("name")] public string Name;
 	[JsonProperty("author")] public string Author;
+	
+	[JsonProperty("announcer")] public SceneDescriptor_Announcer AnnouncerLines;
+	private bool disposedValue;
 
 	public static SceneDescriptor ParseFile(string filepath) => ParseFile<SceneDescriptor>(filepath);
+
+	protected virtual void Dispose(bool disposing) {
+		if (!disposedValue) {
+			if (disposing) {
+				// TODO: dispose managed state (managed objects)
+			}
+
+			// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+			// TODO: set large fields to null
+			disposedValue = true;
+		}
+	}
+
+	~SceneDescriptor()
+	{
+	    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+	    Dispose(disposing: false);
+	}
+
+	public void Dispose() {
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
 }
