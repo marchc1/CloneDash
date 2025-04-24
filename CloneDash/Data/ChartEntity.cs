732a271f51c73353b6d8e8a0ec7815e53880d6cf
march <106459595+marchc1@users.noreply.github.com>
2025-04-23 19:02:56 -0700

    Update ChartEntity.cs

--- CloneDash/Data/ChartEntity.cs
+++ CloneDash/Data/ChartEntity.cs
@@ -2,22 +2,56 @@
 {
     public class ChartEntity
     {
+		/// <summary>
+		/// Entity type
+		/// </summary>
         public EntityType Type;
+		/// <summary>
+		/// Entity pathway
+		/// </summary>
         public PathwaySide Pathway;
+		/// <summary>
+		/// Vertical direction (or right-side) for this entity.
+		/// </summary>
         public EntityEnterDirection EnterDirection;
+		/// <summary>
+		/// Dependant on entity logic and only really applciable to <see cref="Game.Entities.SingleHitEnemy"/>
+		/// </summary>
         public EntityVariant Variant;
 
         public double HitTime;
         public double ShowTime;
-        public bool Flipped;
+		public double Length;
 
+		/// <summary>
+		/// How much fever this entity can give to the palyer.
+		/// </summary>
         public int Fever;
+		/// <summary>
+		/// How much score this entity can give to the player.
+		/// </summary>
         public int Score;
+		/// <summary>
+		/// How fast (from a range of 1 -> 3) does the entity go. This eventually gets plugged into the speeds parameters
+		/// of whatever pathway animation is played
+		/// </summary>
         public int Speed;
+		/// <summary>
+		/// How much health this entity can give to the player.
+		/// </summary>
         public int Health;
+		/// <summary>
+		/// How much damage this entity can deal to the player.
+		/// </summary>
         public int Damage;
-        internal double Length;
 
+		/// <summary>
+		/// Is the entity vertically flipped.
+		/// </summary>
+        public bool Flipped;
+		/// <summary>
+		/// Is the entity related to the boss in any way.
+		/// </summary>
         public bool RelatedToBoss;
 
         public string? DebuggingInfo { get; internal set; }
