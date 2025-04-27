94c6bb7080eab3b684e9d31a4228b557c270140f
march <106459595+marchc1@users.noreply.github.com>
2025-04-26 23:31:12 -0700

    Create CD_SceneEdit.cs

---
+++ CloneDash/Levels/CD_SceneEdit.cs
@@ -0,0 +1,55 @@
+﻿using CloneDash.Game;
+using Nucleus;
+using Nucleus.Core;
+using Nucleus.Engine;
+using Nucleus.Types;
+using Nucleus.UI;
+using Nucleus.UI.Elements;
+using static AssetStudio.BundleFile;
+
+namespace CloneDash.Levels;
+
+public class CD_SceneEdit() : CD_GameLevel(null)
+{
+	public static ConCommand clonedash_sceneedit = ConCommand.Register(nameof(clonedash_sceneedit), (_, _) => {
+		Interlude.Begin("Loading the Scene Editor...");
+		Interlude.Spin();
+		EngineCore.LoadLevel(new CD_SceneEdit());
+	}, "Opens the scene editor");
+
+	Menubar menubar;
+
+	public override void Initialize(params object[] args) {
+		base.Initialize(args);
+
+		var bgrEditWindow = UI.Add<Window>();
+		bgrEditWindow.Title = "Lua - Background Renderer";
+		bgrEditWindow.Size = new(1200, 820);
+		bgrEditWindow.Center();
+
+		bgrEditWindow.Add(out TextEditor bgrEdit);
+		bgrEdit.Dock = Dock.Fill;
+		bgrEdit.Highlighter = new LuaSyntaxHighlighter();
+		bgrEdit.SetText(Filesystem.ReadAllText("scene", "scripts/background.lua"));
+		bgrEdit.TextSize = 14;
+		menubar = UI.Add<Menubar>();
+		menubar.Dock = Dock.Top;
+		menubar.BackgroundColor = new(10, 15, 20, 155);
+		menubar.MoveToBack();
+
+		var options = menubar.AddButton("Scene");
+		options.AddButton("Refresh Scene", null, () => ConCommand.Execute(clonedash_sceneedit));
+
+		ConsoleSystem.AddScreenBlocker(this.UI);
+	}
+
+	public override void PreRender(FrameState frameState) {
+		base.PreRender(frameState);
+	}
+
+	public override void PostRender(FrameState frameState) {
+		base.PostRender(frameState);
+		ConsoleSystem.TextSize = 11;
+		ConsoleSystem.RenderToScreen(4 + 6, (int)(menubar.RenderBounds.H + 10));
+	}
+}
