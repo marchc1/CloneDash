abbf8f2002c00722a27b2533a3c53b53fd705ccf
march <106459595+marchc1@users.noreply.github.com>
2025-04-30 13:58:58 -0700

    Update ChartSheet.cs

--- CloneDash/Data/ChartSheet.cs
+++ CloneDash/Data/ChartSheet.cs
@@ -6,7 +6,7 @@
         public ChartSheet(ChartSong song) => Song = song;
 
         public double StartOffset { get; set; }
-        public int Difficulty { get; set; }
+        public string Rating { get; set; }
         public List<ChartEntity> Entities { get; } = [];
         public List<ChartEvent> Events { get; } = [];
     }
