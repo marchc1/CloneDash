e78ed591c66d08c5edc082789b8bbd3f13acacf1
march <106459595+marchc1@users.noreply.github.com>
2025-04-30 13:58:50 -0700

    Add more stuff to CD_Statistics

--- CloneDash/Levels/CD_Statistics.cs
+++ CloneDash/Levels/CD_Statistics.cs
@@ -1,16 +1,25 @@
 ﻿using CloneDash.Data;
 using CloneDash.Game.Statistics;
+using CloneDash.Modding.Descriptors;
+using CloneDash.Modding.Settings;
+using Nucleus;
 using Nucleus.Core;
 using Nucleus.Engine;
+using Nucleus.Models.Runtime;
+using Nucleus.Types;
 using Nucleus.UI;
+using Raylib_cs;
 
 namespace CloneDash.Levels
 {
 	public class CD_Statistics : Level
-    {
+	{
 		ChartSheet sheet;
 		CD_StatisticsData stats;
-        public override void Initialize(params object[] args) {
+		CharacterDescriptor character;
+		ModelInstance model;
+		AnimationHandler anims;
+		public override void Initialize(params object[] args) {
 #nullable disable
 			sheet = args[0] as ChartSheet;
 			stats = args[1] as CD_StatisticsData;
@@ -19,18 +28,50 @@ namespace CloneDash.Levels
 			if (sheet == null) throw new NullReferenceException(nameof(sheet));
 			if (stats == null) throw new NullReferenceException(nameof(stats));
 
+			CharacterDescriptor character = CharacterMod.GetCharacterData();
+			if (character == null) return;
+			if (character.Filename == null) return;
+			this.character = character;
+
+			model = Models.CreateInstanceFromFile("character", character.GetVictoryModel());
+			anims = new(model.Data);
+			anims.SetAnimation(0, character.Victory.Standby, true);
+
 			stats.Compute();
 
 			var tempPanel = UI.Add<Panel>();
 			tempPanel.Dock = Dock.Fill;
 			tempPanel.PaintOverride += TempPanel_PaintOverride;
-        }
+
+			Keybinds.AddKeybind([KeyboardLayout.USA.LeftControl, KeyboardLayout.USA.R], () => {
+				EngineCore.LoadLevel(new CD_Statistics(), sheet, stats);
+			});
+
+			var bottom = UI.Add<Panel>();
+			bottom.DrawPanelBackground = false;
+
+			bottom.DynamicallySized = true;
+			bottom.Size = new(0.07f);
+			bottom.Dock = Dock.Bottom;
+
+			var restart = bottom.Add<Button>();
+			restart.DynamicallySized = true;
+			restart.Size = new(.2f);
+			restart.Text = "Restart";
+			restart.Dock = Dock.Left;
+
+			var back = bottom.Add<Button>();
+			back.DynamicallySized = true;
+			back.Size = new(.2f);
+			back.Text = "Main Menu";
+			back.Dock = Dock.Right;
+		}
 
 		private void TempPanel_PaintOverride(Element self, float width, float height) {
 			stats.Compute();
 			var y = 0;
 			string[] lines = [
-				$"Statistics information:",
+				$"[{sheet.Rating}] -  {sheet.Song.Name}",
 				$"      Grade: {stats.Grade}",
 				$"      Accuracy: {stats.Accuracy}",
 				$"      Score: {stats.Score}",
@@ -49,10 +90,32 @@ namespace CloneDash.Levels
 			];
 			Graphics2D.SetDrawColor(255, 255, 255);
 			var fs = 24;
-			foreach(var line in lines) {
+			foreach (var line in lines) {
 				Graphics2D.DrawText(16, 16 + y, line, "Noto Sans", fs);
 				y += fs + 4;
 			}
+		}
+
+		public override void Think(FrameState frameState) {
+			base.Think(frameState);
+			if (model != null) {
+				model.Position = new(frameState.WindowWidth / 2, (1 - (float)NMath.Ease.OutElastic(Math.Clamp(Curtime * 0.2, 0, 1))) * (frameState.WindowHeight));
+
+				anims?.AddDeltaTime(CurtimeDelta);
+				anims?.Apply(model);
+			}
+		}
+
+		public override void Render2D(FrameState frameState) {
+			base.Render2D(frameState);
+			Raylib.BeginMode2D(new() {
+				Zoom = frameState.WindowHeight / 900 / 2.4f,
+				Offset = new(frameState.WindowWidth / 2 - frameState.WindowWidth * .2f, frameState.WindowHeight / 1)
+			});
+
+			model?.Render();
+
+			Raylib.EndMode2D();
 		}
 	}
 }
