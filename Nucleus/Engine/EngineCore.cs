d35dd3e88b30db73303f966d86a4d8cd1ce3ec50
march <106459595+marchc1@users.noreply.github.com>
2025-06-10 00:50:55 -0700

    some moving classes around 1 am idea

--- Nucleus/Engine/EngineCore.cs
+++ Nucleus/Engine/EngineCore.cs
@@ -17,11 +17,6 @@ using Nucleus.Util;
 
 namespace Nucleus;
 
-/// <summary>
-/// Marks the class as needing to be statically constructed during the engine initialization. Will fix most convar/concommand issues.
-/// </summary>
-[AttributeUsage(AttributeTargets.Class)]
-public class MarkForStaticConstructionAttribute : Attribute;
 [MarkForStaticConstruction]
 public static class EngineCore
 {
