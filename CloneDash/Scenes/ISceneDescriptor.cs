0a4329d8c2db3dc4b15aae9d5f4fb85170dabfb9
march <106459595+marchc1@users.noreply.github.com>
2025-06-22 00:38:33 -0700

    Separate types

--- CloneDash/Scenes/ISceneDescriptor.cs
+++ CloneDash/Scenes/ISceneDescriptor.cs
@@ -5,26 +5,6 @@ using Nucleus.ManagedMemory;
 using Nucleus.Models.Runtime;
 
 namespace CloneDash.Scenes;
-
-public enum SceneSound {
-	Begin,
-	Fever,
-	Unpause,
-	FullCombo,
-
-	Mash,
-	HP,
-	Score,
-	Jump,
-	EmptyAttack,
-	EmptyJump,
-	Loud1,
-	Loud2,
-	Medium1,
-	Medium2,
-	Quiet,
-	PressTop
-}
 public interface ISceneDescriptor
 {
 	public void Initialize(CD_GameLevel game);
