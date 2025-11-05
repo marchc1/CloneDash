c84c2115e6f2ec2ab3106830bc5e128021ae5570
march <106459595+marchc1@users.noreply.github.com>
2025-11-05 11:11:27 -0800

    Add wordwrap for labels

--- Nucleus/UI/Elements/Label.cs
+++ Nucleus/UI/Elements/Label.cs
@@ -2,6 +2,9 @@
 using Nucleus.Extensions;
 using Nucleus.Types;
 
+using System;
+using System.Diagnostics;
+
 namespace Nucleus.UI
 {
 	public class Label : Element
@@ -17,7 +20,14 @@ namespace Nucleus.UI
 			}
 		}
 
-		public Vector2F TextPadding { get; set; } = new(0);
+		Vector2F textPadding;
+		public Vector2F TextPadding {
+			get => textPadding;
+			set {
+				textPadding = value;
+				InvalidateText();
+			}
+		}
 
 		public override void TextChanged(string oldText, string newText) {
 			base.TextChanged(oldText, newText);
@@ -28,12 +38,31 @@ namespace Nucleus.UI
 
 		struct TextRange
 		{
+			public string OriginalText;
 			public int Start;
 			public int End;
 			public float Width;
 			public float Height;
+
+			public int Length => (End - Start);
+
+			public override string ToString() {
+				return $"'{OriginalText.AsSpan()[Start..End]}' (range: {Start} -> {End}, size: {Width}x{Height})";
+			}
 		}
 		readonly List<TextRange> textRanges = [];
+		Vector2F fullTextSize;
+		float textHeightOffset;
+
+		bool wordWrap;
+		public bool WordWrap {
+			get => wordWrap;
+			set {
+				if (wordWrap != value)
+					InvalidateText();
+				wordWrap = value;
+			}
+		}
 
 		bool textInvalid = true;
 		private void InvalidateText() {
@@ -44,27 +73,75 @@ namespace Nucleus.UI
 				return;
 
 			textRanges.Clear();
-			int x = 0;
+			fullTextSize = default;
+
+			if(wordWrap == false) {
+				return;
+			}
+
+			int y = 0;
 
 			int startPos = 0;
 			int endPos = 0;
 
+
 			ReadOnlySpan<char> text = Text;
-			TextRange workingRange = default;
+			TextRange workingRange = new() { OriginalText = Text };
 			Vector2F workingArea = RenderBounds.Size - TextPadding;
+
 			int wordPos = 0;
 			int spacePos;
-			while (wordPos < text.Length && (spacePos = text[wordPos..].IndexOf(' ')) != -1) {
-				// Get one word
-				ReadOnlySpan<char> word = text[wordPos..spacePos];
-				Vector2F wordSize = Graphics2D.GetTextSize(word, Font, TextSize);
+
+			void pushWorkingRange() {
+				if (textRanges.Count >= 1)
+					textHeightOffset += textRanges[textRanges.Count - 1].Height;
+				textRanges.Add(workingRange);
+
+				fullTextSize.W = Math.Max(fullTextSize.W, workingRange.Width);
+				fullTextSize.H += workingRange.Height;
+
+				workingRange = default;
+				workingRange.Start = wordPos;
+				workingRange.End = wordPos;
+				workingRange.OriginalText = Text;
 			}
+
+			while (wordPos < text.Length) {
+				spacePos = text[wordPos..].IndexOf(' ');
+				bool lastWord = spacePos == -1;
+				if (lastWord)
+					spacePos = text.Length - wordPos;
+
+				ReadOnlySpan<char> word = text[wordPos..(wordPos + spacePos)];
+				Vector2F wordSize = Graphics2D.GetTextSize(word, Font, TextSize);
+
+				if (workingRange.Width > 0 && (workingRange.Width + wordSize.W) > workingArea.W)
+					pushWorkingRange();
+
+				workingRange.Width += wordSize.W;
+
+				if (!lastWord)
+					workingRange.Width += Graphics2D.GetTextSize(" ", Font, TextSize).W;
+
+				workingRange.Height = Math.Max(wordSize.H, workingRange.Height);
+				workingRange.End += word.Length + (lastWord ? 0 : 1);
+
+				wordPos += spacePos + 1;
+			}
+
+
+			if (workingRange.Length > 0)
+				pushWorkingRange();
 
 			textInvalid = false;
 		}
 
 		protected override void Initialize() {
 			base.Initialize();
+		}
+		protected override void PerformLayout(float width, float height) {
+			base.PerformLayout(width, height);
+			InvalidateText();
 		}
 		protected override void ModifyLayout(ref RectangleF renderBounds) {
 			if (!AutoSize)
@@ -96,9 +173,29 @@ namespace Nucleus.UI
 				textC = textC.Adjust(0, 0, -0.5f);
 			}
 
-			Vector2F textDrawingPosition = TextAlignment.GetPositionGivenAlignment(RenderBounds.Size, TextPadding);
+			Span<TextRange> ranges = textRanges.AsSpan();
+			Vector2F startDrawingPosition = TextAlignment.GetPositionGivenAlignment(RenderBounds.Size, TextPadding);
+			TextAlignment vertical = TextAlignment.ToTextAlignment().vertical;
+
 			Graphics2D.SetDrawColor(textC);
-			Graphics2D.DrawText(textDrawingPosition, Text, Font, TextSize, TextAlignment);
+
+			if (ranges.Length == 0) {
+				Graphics2D.DrawText(startDrawingPosition, Text, Font, TextSize, TextAlignment);
+				return;
+			}
+
+			if (ranges.Length > 1) {
+				if (vertical == Types.TextAlignment.Center)
+					startDrawingPosition.Y -= textHeightOffset / 2;
+				else if (vertical == Types.TextAlignment.Bottom)
+					startDrawingPosition.Y -= textHeightOffset;
+			}
+
+			
+			foreach (var range in ranges) {
+				Graphics2D.DrawText(startDrawingPosition, Text.AsSpan()[range.Start..range.End], Font, TextSize, TextAlignment);
+				startDrawingPosition.Y += range.Height;
+			}
 		}
 	}
 }
