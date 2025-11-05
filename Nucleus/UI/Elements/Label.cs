75d5d1179703f99651fd1bc91e39b91efbeabc9f
march <106459595+marchc1@users.noreply.github.com>
2025-11-05 13:53:00 -0800

    Add different modes for text overflow
    We now have 5 modes:
    
    1. Nothing (default)
    2. Word wrap
    where words
    stay within
    the render
    bounds
    3. Character w
    rap where cha
    racters stay wi
    thin the rende
    r bounds
    4. Word truncate where words get cut off if they...
    5. Character truncate where characters get cut o...

--- Nucleus/UI/Elements/Label.cs
+++ Nucleus/UI/Elements/Label.cs
@@ -5,108 +5,157 @@ using Nucleus.Types;
 using System;
 using System.Diagnostics;
 
-namespace Nucleus.UI
+namespace Nucleus.UI;
+
+/// <summary>
+/// Word wrap mode
+/// </summary>
+public enum TextOverflowMode
 {
-	public class Label : Element
-	{
-		public bool DrawBackground { get; set; } = false;
-		public Anchor TextAlignment { get; set; } = Anchor.Center;
-		private bool __autosize = false;
-		public bool AutoSize {
-			get { return __autosize; }
-			set {
-				__autosize = value;
-				InvalidateLayout();
-			}
-		}
+	/// <summary> Do nothing. </summary>
+	None,
+	/// <summary> When a word would overflow the bounds of the right side of the element, start the remainder of the text on a new line, until no text remains.</summary>
+	WordWrap,
+	/// <summary> When a character would overflow the bounds of the right side of the element, start the remainder of the text on a new line, until no text remains.</summary>
+	CharWrap,
+	/// <summary> When a word would overflow the bounds of the right side of the element, truncate the end of the string with a '...' </summary>
+	WordTruncate,
+	/// <summary> When a character would overflow the bounds of the right side of the element, truncate the end of the string with a '...' </summary>
+	CharTruncate,
+}
 
-		Vector2F textPadding;
-		public Vector2F TextPadding {
-			get => textPadding;
-			set {
-				textPadding = value;
-				InvalidateText();
-			}
-		}
+public static class TextOverflowModeTools
+{
+	public static bool IsWrap(this TextOverflowMode textOverflowMode) => textOverflowMode switch {
+		TextOverflowMode.WordWrap or TextOverflowMode.CharWrap => true,
+		_ => false
+	};
+	public static bool IsTruncate(this TextOverflowMode textOverflowMode) => textOverflowMode switch {
+		TextOverflowMode.WordTruncate or TextOverflowMode.CharTruncate => true,
+		_ => false
+	};
+	public static bool TargetsWord(this TextOverflowMode textOverflowMode) => textOverflowMode switch {
+		TextOverflowMode.WordWrap or TextOverflowMode.WordTruncate => true,
+		_ => false
+	};
+	public static bool TargetsCharacter(this TextOverflowMode textOverflowMode) => textOverflowMode switch {
+		TextOverflowMode.CharTruncate or TextOverflowMode.CharWrap => true,
+		_ => false
+	};
+}
 
-		public override void TextChanged(string oldText, string newText) {
-			base.TextChanged(oldText, newText);
-			if (__autosize)
-				InvalidateLayout();
+
+public class Label : Element
+{
+	public bool DrawBackground { get; set; } = false;
+	public Anchor TextAlignment { get; set; } = Anchor.Center;
+	private bool __autosize = false;
+	public bool AutoSize {
+		get { return __autosize; }
+		set {
+			__autosize = value;
+			InvalidateLayout();
+		}
+	}
+
+	Vector2F textPadding;
+	public Vector2F TextPadding {
+		get => textPadding;
+		set {
+			textPadding = value;
 			InvalidateText();
 		}
+	}
 
-		struct TextRange
-		{
-			public string OriginalText;
-			public int Start;
-			public int End;
-			public float Width;
-			public float Height;
+	public override void TextChanged(string oldText, string newText) {
+		base.TextChanged(oldText, newText);
+		if (__autosize)
+			InvalidateLayout();
+		InvalidateText();
+	}
 
-			public int Length => (End - Start);
+	struct TextRange
+	{
+		public string OriginalText;
+		public int Start;
+		public int End;
+		public float Width;
+		public float Height;
+		public bool Truncate;
+		public string TruncateText;
 
-			public override string ToString() {
-				return $"'{OriginalText.AsSpan()[Start..End]}' (range: {Start} -> {End}, size: {Width}x{Height})";
-			}
+		public int Length => (End - Start);
+
+		public override string ToString() {
+			return $"'{OriginalText.AsSpan()[Start..End]}' (range: {Start} -> {End}, size: {Width}x{Height})";
 		}
-		readonly List<TextRange> textRanges = [];
-		Vector2F fullTextSize;
+	}
+	readonly List<TextRange> textRanges = [];
+	Vector2F fullTextSize;
 
-		bool wordWrap;
-		public bool WordWrap {
-			get => wordWrap;
-			set {
-				if (wordWrap != value)
-					InvalidateText();
-				wordWrap = value;
+	TextOverflowMode textOverflowMode;
+	public TextOverflowMode TextOverflowMode {
+		get => textOverflowMode;
+		set {
+			if (textOverflowMode != value)
+				InvalidateText();
+			textOverflowMode = value;
+		}
+	}
+
+	bool textInvalid = true;
+	private void InvalidateText() {
+		textInvalid = true;
+	}
+	private void ValidateText() {
+		if (!textInvalid)
+			return;
+
+		textRanges.Clear();
+		fullTextSize = default;
+
+		if (textOverflowMode == TextOverflowMode.None)
+			return;
+
+		ReadOnlySpan<char> text = Text;
+		TextRange workingRange = new() { OriginalText = Text };
+		Vector2F workingArea = RenderBounds.Size - TextPadding - new Vector2F(4, 4);
+
+		if (textOverflowMode.IsTruncate())
+			workingArea.W -= Graphics2D.GetTextSize("...", Font, TextSize).X;
+
+		int wordPos = 0;
+
+		bool pushWorkingRange(bool notForced = false) {
+			if (workingRange.Length > 0)
+				workingRange.End -= textOverflowMode.TargetsWord() ? 1 : 0;
+
+			fullTextSize.W = Math.Max(fullTextSize.W, workingRange.Width);
+			fullTextSize.H += workingRange.Height;
+
+			bool truncating = (textOverflowMode.IsTruncate() ||
+							 (textOverflowMode.IsWrap() && fullTextSize.H > workingArea.H)) && !notForced;
+
+			workingRange.Truncate = truncating;
+			if (truncating) {
+				workingRange.TruncateText = $"{Text.AsSpan()[workingRange.Start..workingRange.End]}...";
+				workingRange.Width += Graphics2D.GetTextSize("...", Font, TextSize).W;
 			}
+
+			textRanges.Add(workingRange);
+
+			workingRange = new TextRange {
+				OriginalText = Text,
+				Start = wordPos,
+				End = wordPos
+			};
+
+			return !truncating;
 		}
 
-		bool textInvalid = true;
-		private void InvalidateText() {
-			textInvalid = true;
-		}
-		private void ValidateText() {
-			if (!textInvalid)
-				return;
-
-			textRanges.Clear();
-			fullTextSize = default;
-
-			if (wordWrap == false) {
-				return;
-			}
-
-			int y = 0;
-
-			int startPos = 0;
-			int endPos = 0;
-
-			ReadOnlySpan<char> text = Text;
-			TextRange workingRange = new() { OriginalText = Text };
-			Vector2F workingArea = RenderBounds.Size - TextPadding - new Vector2F(4, 4);
-
-			int wordPos = 0;
-			int spacePos;
-
-			void pushWorkingRange() {
-				if (workingRange.Length > 0)
-					workingRange.End -= 1; // Cut off the space
-
-				textRanges.Add(workingRange);
-
-				fullTextSize.W = Math.Max(fullTextSize.W, workingRange.Width);
-				fullTextSize.H += workingRange.Height;
-
-				workingRange = default;
-				workingRange.Start = wordPos;
-				workingRange.End = wordPos;
-				workingRange.OriginalText = Text;
-			}
-
-			while (wordPos < text.Length) {
-				spacePos = text[wordPos..].IndexOf(' ');
+		while (wordPos < text.Length) {
+			if (textOverflowMode.TargetsWord()) {
+				int spacePos = text[wordPos..].IndexOf(' ');
 				bool lastWord = spacePos == -1;
 				if (lastWord)
 					spacePos = text.Length - wordPos;
@@ -115,10 +164,10 @@ namespace Nucleus.UI
 				Vector2F wordSize = Graphics2D.GetTextSize(word, Font, TextSize);
 
 				if (workingRange.Width > 0 && (workingRange.Width + wordSize.W) > workingArea.W)
-					pushWorkingRange();
+					if (!pushWorkingRange())
+						break;
 
 				workingRange.Width += wordSize.W;
-
 				if (!lastWord)
 					workingRange.Width += Graphics2D.GetTextSize(" ", Font, TextSize).W;
 
@@ -127,74 +176,89 @@ namespace Nucleus.UI
 
 				wordPos += spacePos + 1;
 			}
+			else {
+				char c = text[wordPos];
+				Vector2F charSize = Graphics2D.GetTextSize(text.Slice(wordPos, 1), Font, TextSize);
 
+				if (workingRange.Width > 0 && (workingRange.Width + charSize.W) > workingArea.W)
+					if (!pushWorkingRange())
+						break;
 
-			if (workingRange.Length > 0)
-				pushWorkingRange();
-
-			textInvalid = false;
-		}
-
-		protected override void Initialize() {
-			base.Initialize();
-		}
-		protected override void PerformLayout(float width, float height) {
-			base.PerformLayout(width, height);
-			InvalidateText();
-		}
-		protected override void ModifyLayout(ref RectangleF renderBounds) {
-			if (!AutoSize)
-				return;
-
-			Vector2F textSize = Graphics2D.GetTextSize(Text, Font, TextSize);
-
-			renderBounds.W = textSize.X + TextPadding.X;
-			renderBounds.H = textSize.Y + TextPadding.Y;
-
-			if (!DockMargin.IsZero) {
-				renderBounds.W += (DockMargin.X + DockMargin.W) * 2;
-				renderBounds.H += (DockMargin.Y + DockMargin.H) * 2;
-			}
-			if (!Parent.DockPadding.IsZero) {
-				renderBounds.W += (Parent.DockPadding.X + Parent.DockPadding.W) * 2;
-				renderBounds.H += (Parent.DockPadding.Y + Parent.DockPadding.H) * 2;
+				workingRange.Width += charSize.W;
+				workingRange.Height = Math.Max(charSize.H, workingRange.Height);
+				workingRange.End++;
+				wordPos++;
 			}
 		}
-		public override void Paint(float width, float height) {
-			ValidateText();
 
-			if (DrawBackground) {
-				Graphics2D.SetDrawColor(BackgroundColor);
-				Graphics2D.DrawRectangle(0, 0, width, height);
-			}
-			var textC = TextColor;
-			if (!CanInput()) {
-				textC = textC.Adjust(0, 0, -0.5f);
-			}
+		if (workingRange.Length > 0)
+			pushWorkingRange(true);
 
-			Span<TextRange> ranges = textRanges.AsSpan();
-			Vector2F startDrawingPosition = TextAlignment.GetPositionGivenAlignment(RectangleF.FromPosAndSize(new(0), new(width, height)), TextPadding);
-			TextAlignment vertical = TextAlignment.ToTextAlignment().vertical;
-
-			Graphics2D.SetDrawColor(textC);
-
-			if (ranges.Length == 0) {
-				Graphics2D.DrawText(startDrawingPosition, Text, Font, TextSize, TextAlignment);
-				return;
-			}
-
-			if (ranges.Length > 1) {
-				if (vertical == Types.TextAlignment.Center)
-					startDrawingPosition.Y -= (fullTextSize.H - ranges[0].Height) / 2;
-				else if (vertical == Types.TextAlignment.Bottom)
-					startDrawingPosition.Y -= fullTextSize.H - ranges[0].Height;
-			}
+		textInvalid = false;
+	}
 
 
-			foreach (var range in ranges) {
-				Graphics2D.DrawText(startDrawingPosition, Text.AsSpan()[range.Start..range.End], Font, TextSize, TextAlignment);
-				startDrawingPosition.Y += range.Height;
-			}
+	protected override void Initialize() {
+		base.Initialize();
+	}
+	protected override void PerformLayout(float width, float height) {
+		base.PerformLayout(width, height);
+		InvalidateText();
+	}
+	protected override void ModifyLayout(ref RectangleF renderBounds) {
+		if (!AutoSize)
+			return;
+
+		Vector2F textSize = Graphics2D.GetTextSize(Text, Font, TextSize);
+
+		renderBounds.W = textSize.X + TextPadding.X;
+		renderBounds.H = textSize.Y + TextPadding.Y;
+
+		if (!DockMargin.IsZero) {
+			renderBounds.W += (DockMargin.X + DockMargin.W) * 2;
+			renderBounds.H += (DockMargin.Y + DockMargin.H) * 2;
+		}
+		if (!Parent.DockPadding.IsZero) {
+			renderBounds.W += (Parent.DockPadding.X + Parent.DockPadding.W) * 2;
+			renderBounds.H += (Parent.DockPadding.Y + Parent.DockPadding.H) * 2;
+		}
+	}
+	public override void Paint(float width, float height) {
+		ValidateText();
+
+		if (DrawBackground) {
+			Graphics2D.SetDrawColor(BackgroundColor);
+			Graphics2D.DrawRectangle(0, 0, width, height);
+		}
+		var textC = TextColor;
+		if (!CanInput()) {
+			textC = textC.Adjust(0, 0, -0.5f);
+		}
+
+		Span<TextRange> ranges = textRanges.AsSpan();
+		Vector2F startDrawingPosition = TextAlignment.GetPositionGivenAlignment(RectangleF.FromPosAndSize(new(0), new(width, height)), TextPadding);
+		TextAlignment vertical = TextAlignment.ToTextAlignment().vertical;
+
+		Graphics2D.SetDrawColor(textC);
+
+		if (ranges.Length == 0) {
+			Graphics2D.DrawText(startDrawingPosition, Text, Font, TextSize, TextAlignment);
+			return;
+		}
+
+		if (ranges.Length > 1) {
+			if (vertical == Types.TextAlignment.Center)
+				startDrawingPosition.Y -= (fullTextSize.H - ranges[0].Height) / 2;
+			else if (vertical == Types.TextAlignment.Bottom)
+				startDrawingPosition.Y -= fullTextSize.H - ranges[0].Height;
+		}
+
+		foreach (var range in ranges) {
+			ReadOnlySpan<char> subtext = range.Truncate ? range.TruncateText : Text.AsSpan()[range.Start..range.End];
+			Graphics2D.DrawText(startDrawingPosition, subtext, Font, TextSize, TextAlignment);
+			if (range.Truncate)
+				break;
+			startDrawingPosition.Y += range.Height;
 		}
 	}
 }
