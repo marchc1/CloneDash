0fcbbd92b5995bd00f2f3c309a68c6832607be6b
march <106459595+marchc1@users.noreply.github.com>
2026-01-30 01:32:21 -0800

    Update IManagedMemory.cs

--- Nucleus/ManagedMemory/IManagedMemory.cs
+++ Nucleus/ManagedMemory/IManagedMemory.cs
@@ -110,11 +110,12 @@ public interface IManagedMemory : IValidatable, IDisposable
 	/// <param name="path"></param>
 	/// <returns></returns>
 	public static int MergePathSize(ReadOnlySpan<char> pathID, ReadOnlySpan<char> path) => pathID.Length + path.Length + 1;
-	public static void MergePath(ReadOnlySpan<char> pathID, ReadOnlySpan<char> path, Span<char> output) {
+	public static Span<char> MergePath(ReadOnlySpan<char> pathID, ReadOnlySpan<char> path, Span<char> output) {
 		if (output.Length < (pathID.Length + path.Length + 1))
 			throw new Exception("Did not allocate enough space for this operation!");
 		pathID.CopyTo(output[0..]);
 		output[pathID.Length] = '@';
 		path.CopyTo(output[(pathID.Length + 1)..]);
+		return output[(pathID.Length + path.Length + 1)..];
 	}
 }
