8a095dcc14c0ce4e0822314a365fead1db10c9c8
march <106459595+marchc1@users.noreply.github.com>
2026-02-11 21:01:51 -0800

    Graphics/window interface scaffolding

---
+++ Nucleus.Common/Graphics/GraphicsAPIVersion.cs
@@ -0,0 +1,10 @@
+﻿namespace Nucleus.Common.Graphics;
+
+public enum GraphicsAPIVersion : ulong{
+	// Last 16 bits reserved for API type
+	OpenGL = 1 << 48,
+
+	OpenGL_3_30 = OpenGL | 330,
+	OpenGL_4_30 = OpenGL | 430,
+	// OpenGL v4.60 would be nice one day!
+}
