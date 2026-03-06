0a9df046130713087b04996ebddab194bfcf4daa
march <106459595+marchc1@users.noreply.github.com>
2026-03-05 23:16:57 -0800

    fix text effects (for now)
    Will eventually make these use their MD sprite equivs and rewrite them completely

--- CloneDash/Game/TextEffect.cs
+++ CloneDash/Game/TextEffect.cs
@@ -45,15 +45,17 @@ namespace CloneDash.Game
 				}
 			}
 
+			var finalPos = Position * DashGameLevel.GlobalScale;
 			var pos0to1 = Ease.OutExpo(Raymath.Remap((float)lifetime, 0, ageToDie, 0, 1));
 			var pos0to1_two = TransitionOut == TextEffectTransitionOut.SlideUpThenToLeft ? Ease.InExpo(Raymath.Remap(Math.Clamp((float)lifetime, ageToDie / 2, ageToDie), ageToDie / 2, ageToDie, 0, 1)) : 0;
 
 			var pos = pos0to1 * frameState.WindowHeight * 0.2f;
 			var size = 1f - (float)Ease.InExpo(Remap(lifetime, 0, ageToDie, 0, 1));
 
+			// TODO: Move this to a render context! post render isn't good for this
 			Rlgl.PushMatrix();
-			Rlgl.Translatef(frameState.WindowWidth / 2, frameState.WindowHeight / 2, 0);
-			Rlgl.Translatef((Position.X / 2) - (pos0to1_two * (frameState.WindowWidth * 0.15f)), (Position.Y / 2) - pos, 0);
+			Rlgl.Translatef(frameState.WindowWidth / 2f, frameState.WindowHeight / 2f, 0);
+			Rlgl.Translatef((finalPos.X / 2) - (pos0to1_two * (frameState.WindowWidth * 0.15f * DashGameLevel.GlobalScale)), (finalPos.Y / 2) - pos, 0);
 			Rlgl.Scalef(size, size, size);
 			Graphics2D.SetDrawColor(Color, (int)(Color.A * Raymath.Remap((float)lifetime, 0, ageToDie, 1, 0)));
 			Graphics2D.DrawText(new(0), Text, Graphics2D.UI_FONT_NAME, 42, TextAlignment.Center, TextAlignment.Center);
