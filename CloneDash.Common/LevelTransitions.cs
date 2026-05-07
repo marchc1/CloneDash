8215418e894dd9f6332ac8ffae2d2397723e67d1
march <106459595+marchc1@users.noreply.github.com>
2026-05-07 13:43:08 -0700

    Interlude fixes

--- CloneDash.Common/LevelTransitions.cs
+++ CloneDash.Common/LevelTransitions.cs
@@ -1,6 +1,7 @@
 ﻿using CloneDash.Common.Gamemodes;
 using CloneDash.Common.Songs;
 using CloneDash.Menu.Searching;
+using Nucleus;
 
 namespace CloneDash.Common;
 
@@ -11,10 +12,20 @@ public static class LevelTransitions
 	public static event Action<SongSelector, ISong>? OnLoadSongSelector;
 
 	public static void LoadMainMenu() => OnLoadMainMenu?.Invoke();
-	public static void LoadSongChart(ReadOnlySpan<char> interludeText, ISongChart? chart, in GameLoadGenericParameters parms) {
+	public static void LoadSongChart(ReadOnlySpan<char> interludeText, ISongChart? chart, GameLoadGenericParameters parms) {
 		if (chart == null)
 			return;
 
+		if(EngineCore.InLevelFrame){
+			string interludeStr = new(interludeText.SliceNullTerminatedString());
+			MainThread.RunASAP(() => loader(interludeStr, chart, parms));
+		}
+		else{
+			loader(interludeText, chart, parms);
+		}
+	}
+
+	static void loader(ReadOnlySpan<char> interludeText, ISongChart chart, GameLoadGenericParameters parms) {
 		if (!interludeText.IsEmpty)
 			Interlude.Begin(new(interludeText.SliceNullTerminatedString()));
 		OnLoadSongChart?.Invoke(chart, parms);
