817090f54e0e79814bf8745d5e98920a6cc52c37
march <106459595+marchc1@users.noreply.github.com>
2026-05-06 22:13:07 -0700

    Convar fixes

--- CloneDash.Common/Settings/GameSettings.cs
+++ CloneDash.Common/Settings/GameSettings.cs
@@ -12,6 +12,6 @@ UseGamemodeDefaults = -1,
 [MarkForStaticConstruction]
 public static class GameSettings
 {
-	public static ConVar r_deadentityvisibility = new(nameof(r_deadentityvisibility), (int)DeadEntityVisibility.UseGamemodeDefaults, FCvar.Saved, "Controls the visibility of entities post-death, in applicable gamemodes with applicable entities. -1 == gamemode default, 0 == fully visible, 1 == dimmed, and 2 == invisible", 0, 2);
+	public static ConVar r_deadentityvisibility = new(nameof(r_deadentityvisibility), (int)DeadEntityVisibility.UseGamemodeDefaults, FCvar.Saved, "Controls the visibility of entities post-death, in applicable gamemodes with applicable entities. -1 == gamemode default, 0 == fully visible, 1 == dimmed, and 2 == invisible", -1, 2);
 	public static DeadEntityVisibility DeadEntityVisibility => (DeadEntityVisibility)r_deadentityvisibility.GetInt();
 }
