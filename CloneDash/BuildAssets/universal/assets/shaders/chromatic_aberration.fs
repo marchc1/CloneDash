55770a4682b9f24b005a54fea96aeaf3a4617228
march <106459595+marchc1@users.noreply.github.com>
2026-05-15 16:42:28 -0700

    Change chromatic aberration to only X axis

--- CloneDash/BuildAssets/universal/assets/shaders/chromatic_aberration.fs
+++ CloneDash/BuildAssets/universal/assets/shaders/chromatic_aberration.fs
@@ -7,12 +7,11 @@ uniform float uStrength;
 
 void main()
 {
-    vec2 dir = fragTexCoord - 0.5;
-    vec2 offset = dir * uStrength * 0.01;
+    float offset = (fragTexCoord.x - 0.5) * uStrength * 0.01;
 
-    float r = texture(texture0, fragTexCoord - offset).r;
+    float r = texture(texture0, fragTexCoord - vec2(offset, 0.0)).r;
     float g = texture(texture0, fragTexCoord).g;
-    float b = texture(texture0, fragTexCoord + offset).b;
+    float b = texture(texture0, fragTexCoord + vec2(offset, 0.0)).b;
 
     finalColor = vec4(r, g, b, 1.0);
 }
