3d55a234aa122eb4edc3f08101081838e6b21971
march <106459595+marchc1@users.noreply.github.com>
2026-05-21 14:57:02 -0700

    Change Axis again

--- Nucleus.Common/Types/Axis.cs
+++ Nucleus.Common/Types/Axis.cs
@@ -1,7 +1,9 @@
 ﻿namespace Nucleus.Types;
 
-public enum Axis
+public enum Axis : byte
 {
-	Horizontal,
-	Vertical
+	None = 0,
+	Horizontal = 1 << 0,
+	Vertical = 1 << 1,
+	Both = Horizontal | Vertical
 }
