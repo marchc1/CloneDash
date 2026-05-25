3bd3c4c13c47be15689f0a352f8f1fd3617c1043
march <106459595+marchc1@users.noreply.github.com>
2026-05-24 19:26:02 -0700

    Update Label.cs

--- Nucleus/UI/Elements/Visual/Label.cs
+++ Nucleus/UI/Elements/Visual/Label.cs
@@ -194,7 +194,7 @@ public class Label : Element, ITextElement
 	}
 
 
-	public float GetRenderTextSize() => GetDynamicallyScaledFloat(TextSize.Get(), Axis.Vertical);
+	public float GetRenderTextSize() => TextSize.Get() * GetReferenceSize(DynamicTextSizeReference);
 
 	public TextOverflowMode TextOverflowMode {
 		get => textOverflowMode;
