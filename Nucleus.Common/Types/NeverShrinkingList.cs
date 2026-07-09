2b48cd621adbdf3198da252299d0a3c19a76fa5d
march <106459595+marchc1@users.noreply.github.com>
2026-07-09 11:47:11 -0700

    Update NeverShrinkingList.cs

--- Nucleus.Common/Types/NeverShrinkingList.cs
+++ Nucleus.Common/Types/NeverShrinkingList.cs
@@ -2,50 +2,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nucleus.Types;
 
 /// <summary>
-/// A list that never shrinks, which also means its contents can be accessed by reference
+/// A list that never shrinks or reallocates, which also means its contents can be safely accessed by reference
 /// <br/>
 /// Uses fragment-based storage based on <see cref="MAX_FRAGMENT_SIZE"/>
 /// </summary>
-public class NeverShrinkingList<T> : IEnumerable, IEnumerable<T>, ICollection<T>
+public class NeverShrinkingList<T> : IEnumerable, IEnumerable<T?>, ICollection<T?>
 {
 	const int MAX_FRAGMENT_SIZE = 32;
-	List<T[]> fragments = [];
 
+	List<T?[]> fragments = [];
+	int count = 0;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	void fragAddr(int index, out int fragmentAbsIndex, out int fragmentLocalIndex) {
 		fragmentAbsIndex = index / MAX_FRAGMENT_SIZE;
 		fragmentLocalIndex = index % MAX_FRAGMENT_SIZE;
 	}
 
-	int count = 0;
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	bool overflows(int index, out int abs, out int local) {
 		fragAddr(index, out abs, out local);
 		return abs >= fragments.Count || local >= fragments[abs].Length;
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	void allocateFragment(out int abs, out int local) {
 		// Given count, do we need to allocate a fragment
 		if (overflows(count, out abs, out local))
 			fragments.Add(new T[MAX_FRAGMENT_SIZE]);
 	}
 
-	T get(int index) {
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	T? get(int index) {
 		fragAddr(index, out int abs, out int local);
 		return fragments[abs][local];
 	}
 
-	void set(int index, T value) {
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	void set(int index, T? value) {
 		fragAddr(index, out int abs, out int local);
 		fragments[abs][local] = value;
 	}
 
-	public ref T this[int index] {
+	public ref T? this[int index] {
 		get {
 			fragAddr(index, out int abs, out int local);
 			return ref fragments[abs][local];
@@ -55,13 +62,13 @@ public class NeverShrinkingList<T> : IEnumerable, IEnumerable<T>, ICollection<T>
 
 	public bool IsReadOnly => false;
 
-	public void Add(T item) {
+	public void Add(T? item) {
 		allocateFragment(out int abs, out int local);
 		fragments[abs][local] = item;
 		count++;
 	}
 
-	public ref T Add() {
+	public ref T? Add() {
 		allocateFragment(out int abs, out int local);
 		fragments[abs][local] = default;
 		count++;
@@ -69,7 +76,7 @@ public class NeverShrinkingList<T> : IEnumerable, IEnumerable<T>, ICollection<T>
 		return ref fragments[abs][local];
 	}
 
-	public ref T AddNoZero() {
+	public ref T? AddNoZero() {
 		allocateFragment(out int abs, out int local);
 		count++;
 
@@ -87,21 +94,21 @@ public class NeverShrinkingList<T> : IEnumerable, IEnumerable<T>, ICollection<T>
 		count = 0;
 	}
 
-	public bool Contains(T item) => IndexOf(item) != -1;
+	public bool Contains(T? item) => IndexOf(item) != -1;
 
-	public void CopyTo(T[] array, int arrayIndex) {
+	public void CopyTo(T?[] array, int arrayIndex) {
 		for (int i = 0; i < count; i++) {
 			array[arrayIndex++] = this[i];
 		}
 	}
 
-	public IEnumerator<T> GetEnumerator() {
+	public IEnumerator<T?> GetEnumerator() {
 		for (int i = 0; i < count; i++) {
 			yield return get(i);
 		}
 	}
 
-	public int IndexOf(T item) {
+	public int IndexOf(T? item) {
 		for (int i = 0; i < count; i++) {
 			var iT = get(i);
 			if (get(i)?.Equals(item) ?? item == null) return i;
@@ -110,15 +117,15 @@ public class NeverShrinkingList<T> : IEnumerable, IEnumerable<T>, ICollection<T>
 	}
 
 	public void Insert(int index, T item) {
-		throw new NotImplementedException();
+		throw new NotSupportedException("Inserting would break reference guarantees");
 	}
 
-	public bool Remove(T item) {
-		throw new NotImplementedException();
+	public bool Remove(T? item) {
+		throw new NotSupportedException("Removing would break reference guarantees");
 	}
 
 	public void RemoveAt(int index) {
-		throw new NotImplementedException();
+		throw new NotSupportedException("Removing would break reference guarantees");
 	}
 
 	IEnumerator IEnumerable.GetEnumerator() {
